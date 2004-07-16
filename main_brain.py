a767238968f4b26a756709d11492ebfbf3227c68
jbender <jbender@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2004-07-16 21:54:28 +0000

    decrease initial screen size for 1024x768 display
    git-svn-id: https://code.astraw.com/kookaburra/flydra@100 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- main_brain.py
+++ main_brain.py
@@ -25,7 +25,7 @@ class App(wxApp):
     def OnInit(self,*args,**kw):
     
         wxInitAllImageHandlers()
-        frame = wxFrame(None, -1, "Flydra Main Brain",size=(1100,800))
+        frame = wxFrame(None, -1, "Flydra Main Brain",size=(1000,700))
 
         # statusbar ----------------------------------
         self.statusbar = frame.CreateStatusBar()
