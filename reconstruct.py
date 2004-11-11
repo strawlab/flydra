82fea36aba98591da64b091009346a2390eb7f2f
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2004-11-11 20:40:16 +0000

    Example code illustrating how 2D images of point results in 3D point.
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@316 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- reconstruct.py
+++ reconstruct.py
@@ -1,6 +1,7 @@
 import os
 opj=os.path.join
 import numarray as nx
+import sys
 from numarray.linear_algebra import singular_value_decomposition
 
 def load_ascii_matrix(filename):
@@ -10,24 +11,32 @@ def load_ascii_matrix(filename):
     lines = buf.split('\n')[:-1]
     return nx.array([map(float,line.split()) for line in lines])
 
-def save_ascii_matrix(M,filename):
+def save_ascii_matrix(M,fd):
     def fmt(f):
         return '% 8e'%f
     A = nx.asarray(M)
     if len(A.shape) == 1:
         A=nx.reshape(A, (1,A.shape[0]) )
-    fd = open(filename,mode='wb')
+
+    close_file = False
+    if type(fd) == str:
+        fd = open(fd,mode='wb')
+        close_file = True
+        
     for i in range(A.shape[0]):
         buf = ' '.join( map( fmt, A[i,:] ) )
         fd.write( buf )
         fd.write( '\n' )
-    fd.close()
+    if close_file:
+        fd.close()
 
 class Reconstructor:
     def __init__(self,
                  calibration_dir = '/home/astraw/mcsc_data',
                  Pmat_name = 'camera%d.Pmat.cal',
+                 debug = False,
                  ):
+        self._debug = debug
         fd = open(os.path.join(calibration_dir,'camera_order.txt'),'r')
         cam_ids = fd.read().split('\n')
         if cam_ids[-1] == '': del cam_ids[-1] # remove blank line
@@ -37,6 +46,7 @@ class Reconstructor:
         for i, cam_id in enumerate(cam_ids):
             fname = Pmat_name%(i+1)
             self.Pmat[cam_id] = load_ascii_matrix(opj(calibration_dir,fname))
+        self.cam_order = cam_ids
 
     def find3d(self,
                cam_ids_and_points2d):
@@ -50,7 +60,26 @@ class Reconstructor:
             A[2*m  ,:]=xm*row3 - Pmat[0,:]
             A[2*m+1,:]=ym*row3 - Pmat[1,:]
 
+        if self._debug:
+            print 'A', A.shape
+            save_ascii_matrix(A,sys.stdout)
+            print
+        
         u,s,v=singular_value_decomposition(A)
+
+        if self._debug:
+            print 'u',u.shape
+            save_ascii_matrix(u,sys.stdout)
+            print
+
+            print 's',s.shape
+            save_ascii_matrix(s,sys.stdout)
+            print
+
+            print 'v',v.shape
+            save_ascii_matrix(v,sys.stdout)
+            print
+        
         X = v[-1,0:3]/v[-1,3] # normalize
         return X
 
