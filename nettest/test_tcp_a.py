9edf3d93ae5aac8a0212ceac7954c88f2a8b518f
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2005-06-28 22:40:50 +0000

    initial commit of TCP test
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@550 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ nettest/test_tcp_a.py
@@ -0,0 +1,96 @@
+import socket
+import select
+import threading
+import time
+import struct
+
+RMT_HOSTNAME = '192.168.1.151'
+RMT_PORT = 31422
+SERVER_PORT = 31423
+
+fmt = '>id'
+fmt_size = struct.calcsize(fmt)
+
+time_func = time.clock
+
+def server_func():
+    hostname = ''
+
+    # open server port
+    sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockobj.bind((hostname, SERVER_PORT))
+    sockobj.listen(1)
+    sockobj.setblocking(0)
+    listen_sockets = [sockobj]
+    empty_list = []
+    select_select = select.select
+    
+    timeout = 5.0
+
+    client_sockobj = None
+    while client_sockobj is None:
+        print 'SERVER: listening for first connection...'
+        try:
+            in_ready, out_ready, exc_ready = select_select( listen_sockets,
+                                                            empty_list, empty_list, timeout )
+        except select.error, exc:
+            print 'select.error on server socket, ignoring...'
+            continue
+        for sockobj in in_ready:
+            client_sockobj, addr = sockobj.accept()
+            client_sockobj.setblocking(0)
+            print 'SERVER: connected from',addr
+
+    listen_sockets = [client_sockobj]
+    while 1:
+        print 'SERVER: listening for input on client_sockobj...'
+        try:
+            in_ready, out_ready, exc_ready = select_select( listen_sockets,
+                                                            empty_list,
+                                                            empty_list,
+                                                            timeout )
+        except select.error, exc:
+            print 'select.error on server socket, ignoring...'
+            continue
+        except socket.error, exc:
+            print 'socket.error on server socket, ignoring...'
+            continue
+        except Exception, exc:
+            raise
+        except:
+            print 'ERROR: received an exception not derived from Exception'
+            print '-='*10,'I should really quit now!','-='*10
+            continue
+        for sockobj in in_ready:
+            newdata = sockobj.recv(fmt_size)
+            print 'SERVER: got data',repr(newdata)
+            recvtime = time_func()
+            #istr,timestr = newdata.split()
+            istr,timestr = struct.unpack(fmt,newdata)
+            sendtime = float(timestr)
+            #print sendtime, recvtime
+            tdelta = recvtime-sendtime
+            print '%s: %g msec'%(istr,tdelta*1000.0)
+            #print addr,':',newdata
+    print 'server_thread done'
+
+server_thread = threading.Thread(target=server_func)
+server_thread.setDaemon(True)
+server_thread.start()
+
+i=0
+outgoing_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'connecting to remote host...'
+outgoing_socket.connect((RMT_HOSTNAME, RMT_PORT))
+print 'connected'
+try:
+    while 1:
+        i+=1
+        outstr = struct.pack( fmt, i,time_func())
+        #outstr = '%d %s'%(i,repr(time_func()) )
+        outgoing_socket.send(outstr)
+        print 'SENDER: sent',repr(outstr)
+        time.sleep(0.1)
+finally:
+    outgoing_socket.close()
+    
