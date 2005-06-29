9a451257f1632f86a460b716c90b743e522df798
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2005-06-29 17:36:04 +0000

    realtime_viewer mostly works. (could be optimized, though! :)
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@557 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- analysis/realtime_viewer.py
+++ analysis/realtime_viewer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # $Id$
-import numarray as nx
+import Numeric as nx
 import math
 from wxPython.wx import *
 import wxVTKRenderWindow
@@ -9,47 +9,44 @@ from vtk.util.colors import tomato, banana, azure
 import time, sys, threading, socket, struct
 import flydra.reconstruct as reconstruct
 
-hostname = socket.gethostbyname('mainbrain')
+#hostname = socket.gethostbyname('mainbrain')
+hostname = '192.168.1.199'
 sockobj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 port = 28931
 
-listen_socket = sockobj.bind(( hostname, port))
+sockobj.bind(( hostname, port))
 print 'listening on',hostname,port
 
-incoming_data_lock = threading.Lock()
-incoming_data = []
+EVT_TRIG_ID = wxNewId()
+def EVT_TRIG(win, func):
+    win.Connect(-1, -1, EVT_TRIG_ID, func)
+class TrigEvent(wxPyEvent):
+    def __init__(self,data):
+        wxPyEvent.__init__(self)
+        self.SetEventType(EVT_TRIG_ID)
+        self.data = data
 
 class Listener(threading.Thread):
-    def __init__(self):
+    def __init__(self,wxapp):
         threading.Thread.__init__(self)
         self.quit_now = threading.Event()
-##        self.is_done = threading.Event()
-        self.fmt = '<ifffffffffdi'
+        self.fmt = 'ifffffffffd'
+        self.wxapp = wxapp
     def run(self):
-        global incoming_data_lock, incoming_data
         tick = 0
+        print 'listening...'
         while not self.quit_now.isSet():
             data, addr = sockobj.recvfrom(1024)
             tmp = struct.unpack(self.fmt,data)
             corrected_framenumber,x,y,z = tmp[:4]
             line3d = tmp[4:10]
-            timestamp, n_cams = tmp[10:12]
-            #time.sleep(0.5)
-
-            incoming_data_lock.acquire()
-            incoming_data.append( (corrected_framenumber, (x,y,z), line3d, timestamp, n_cams ))
-            incoming_data_lock.release()
-                
-##        self.is_done.set()
+            timestamp = tmp[10]
+            
+            self.wxapp.AddPendingEvent(TrigEvent((corrected_framenumber, (x,y,z), line3d, timestamp )))
+            
     def quit(self):
         self.quit_now.set()
-##        # send packet to self (allow blocking call to quit)
-##        tmp_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-##        tmp_socket.sendto('x'*struct.calcsize(self.fmt),
-##                          ( hostname, port))
-##        self.is_done.wait(0.1)
-##        print
             
 class App(wxApp):
     def OnInit(self,*args,**kw):
@@ -68,7 +65,7 @@ class App(wxApp):
 
         #########################################
 
-        C = nx.array((119, 16, -59))
+        C = nx.array((1250, 150, 470))
         camera = vtkCamera()
         camera.ParallelProjectionOn()
         camera.SetFocalPoint(*C)
@@ -80,7 +77,8 @@ class App(wxApp):
         self.cam1 = camera
         ren.SetActiveCamera( camera )
 
-        ren.SetBackground( 1,1,1)
+        ren.SetBackground( .7,.7,.8 )
+        #ren.SetBackground( 1,1,1)
 
         camera = vtkCamera()
         camera.ParallelProjectionOn()
@@ -94,32 +92,13 @@ class App(wxApp):
         
         self.cam2 = camera
         ren2.SetActiveCamera( camera )
-        ren2.SetBackground( 1,1,1)
+        ren2.SetBackground( .7,.7,.8 )
+        #ren2.SetBackground( 1,1,1)
 
-        #########################################
-    
-        self.current_cog_point = 0
-        self.allocated_cog_points = 0
-
-        self.max_cog_points = 10
-        
-        self.current_line_point = 0
-        self.allocated_line_points = 0
-
-        self.max_line_points = 20
-        
-        self.update_freq_sec = 0.05
-        
-        self.cog_points = vtk.vtkPoints() # 'center of gravity'
-        self.line_points = vtk.vtkPoints()
-        self.lines = vtk.vtkCellArray()
-
-        self.last_update = 0.0
-        
         # point rendering #######################
     
         points_poly_data = vtkPolyData()
-        points_poly_data.SetPoints(self.cog_points)
+#        points_poly_data.SetPoints(self.cog_points)
 
         ball = vtk.vtkSphereSource()
         ball.SetRadius(3.0)
@@ -140,8 +119,8 @@ class App(wxApp):
 
         profileData = vtk.vtkPolyData()
     
-        profileData.SetPoints(self.line_points)
-        profileData.SetLines(self.lines)
+#        profileData.SetPoints(self.line_points)
+#        profileData.SetLines(self.lines)
 
         self.profileTubes = vtk.vtkTubeFilter()
         self.profileTubes.SetNumberOfSides(8)
@@ -162,8 +141,9 @@ class App(wxApp):
 
         # bounding box
         bbox_points = vtkPoints()
-        bbox_points.InsertNextPoint(-200,-100,-150) # minimum values
-        bbox_points.InsertNextPoint(500,450,200) # max
+        datarange = nx.array((200,100,100))
+        bbox_points.InsertNextPoint(C-datarange) # minimum values
+        bbox_points.InsertNextPoint(C+datarange) # max
         bbox_poly_data = vtkPolyData()
         bbox_poly_data.SetPoints(bbox_points)
         bbox_mapper = vtk.vtkPolyDataMapper()
@@ -191,81 +171,96 @@ class App(wxApp):
         ren2.AddProp(axes2)
 
         #########################################
+        self.current_data = []
+        #########################################
 
         ID_Timer  = wxNewId() 	         
         self.timer = wxTimer(self,      # object to send the event to 	 
                              ID_Timer)  # event id to use 	 
-        EVT_TIMER(self,  ID_Timer, self.OnIdle)
-        self.update_interval=30
+        EVT_TIMER(self,  ID_Timer, self.OnTimer)
+        self.update_interval=200
         self.timer.Start(self.update_interval) # call every n msec
-        EVT_IDLE(self.frame, self.OnIdle)
+        #EVT_IDLE(self.frame, self.OnIdle)
+
+        ID_Timer2  = wxNewId() 	         
+        self.timer2 = wxTimer(self,      # object to send the event to 	 
+                              ID_Timer2)  # event id to use 	 
+        EVT_TIMER(self,  ID_Timer2, self.OnDraw)
+        self.timer2.Start(200) # call every n msec
         
         #########################################
-
+        
+        EVT_TRIG(self, self.OnTrig)
+        
         self.frame.Show()
         self.SetTopWindow(self.frame)
 
         return True
-    
-    def OnIdle(self,event):
-        global incoming_data_lock, incoming_data
 
-        incoming_data_lock.acquire()
-        local_data = incoming_data[:] # copy
-        incoming_data = [] # delete
-        incoming_data_lock.release()
+    def OnTrig(self,event):
+        self.current_data.append( event.data )
+        #print 'appending',event.data
+        #print 'len(self.current_data)',len(self.current_data)
 
-        # self.cog_points acts like a FIFO
-        for tmp in local_data:
-            corrected_framenumber, (x,y,z), line3d, timestamp, n_cams = tmp
-            X = nx.array((x,y,z))
-            U = reconstruct.line_direction(line3d)
-            if self.allocated_cog_points < self.max_cog_points:
-                self.cog_points.InsertNextPoint( x,y,z )
-
-                self.current_cog_point += 1
-                self.allocated_cog_points += 1
-                # line
-                self.line_points.InsertNextPoint(*(X+20*U))
-                self.current_line_point += 1
-                self.allocated_line_points += 1
-                
-                self.line_points.InsertNextPoint(*(X-20*U))
-                self.current_line_point += 1
-                self.allocated_line_points += 1
-
-                self.lines.InsertNextCell(2)
-                self.lines.InsertCellPoint(self.current_line_point-2)
-                self.lines.InsertCellPoint(self.current_line_point-1)
+    def OnTimer(self,event):
+        # prune data
+        now = time.time()
+        too_old_sec = 10.0
+        while len(self.current_data):
+            if (now - self.current_data[0][3]) > too_old_sec:
+                # remove data older than too_old_sec seconds
+                val = self.current_data.pop(0)
+                #print 'removing',val
             else:
-                if self.current_cog_point >= self.max_cog_points:
-                    self.current_cog_point = 0
-                self.cog_points.SetPoint( self.current_cog_point, x,y,z )
-                self.current_cog_point += 1
-                # line
-                if self.current_line_point >= self.max_line_points:
-                    self.current_line_point = 0
-                self.line_points.SetPoint(self.current_line_point, *(X+20*U))
-                self.current_line_point += 1
-                
-                self.line_points.SetPoint(self.current_line_point, *(X-20*U))
-                self.current_line_point += 1
-                
-        if len(local_data):
-            now = time.time()
-            if now-self.last_update > self.update_freq_sec: # hand-tuned to prevent CPU overload
-                
-                points_poly_data = vtkPolyData()
-                points_poly_data.SetPoints(self.cog_points)
-                self.balls.SetInput(points_poly_data)
+                # Because the list is ordered, first point that is
+                # recent enough means the rest will be too.
+                break
+        
+    def OnDraw(self,event=None):
 
+        cog_points = vtk.vtkPoints() # 'center of gravity'
+        line_points = vtk.vtkPoints()
+        lines = vtk.vtkCellArray()
+        current_line_point = 0
+
+        N = len(self.current_data)
+        cog_points.SetNumberOfPoints( N )
+
+        #print 'drawing:',len(self.current_data)
+        # self.cog_points acts like a FIFO
+        for i,tmp in enumerate(self.current_data):
+            corrected_framenumber, (x,y,z), line3d, timestamp = tmp
+            X = nx.array((x,y,z))
+            #print '  drawing',X
+
+            # body
+            cog_points.SetPoint(i,x,y,z )
+            #cog_points.InsertNextPoint( x,y,z )
+            if 0:
+                U = reconstruct.line_direction(line3d)
+                
+                # line
+                line_points.InsertNextPoint(*(X+20*U))
+                current_line_point += 1
+                line_points.InsertNextPoint(*(X-20*U))
+                current_line_point += 1
+
+                lines.InsertNextCell(2)
+                lines.InsertCellPoint(current_line_point-2)
+                lines.InsertCellPoint(current_line_point-1)
+        
+        if 1:
+            points_poly_data = vtkPolyData()
+            points_poly_data.SetPoints(cog_points)
+            self.balls.SetInput(points_poly_data)
+
+            if 0:
                 profileData = vtk.vtkPolyData()
-                profileData.SetPoints(self.line_points)
-                profileData.SetLines(self.lines)
+                profileData.SetPoints(line_points)
+                profileData.SetLines(lines)
                 self.profileTubes.SetInput(profileData)
                 
-                self.vtk_render_window.Render()
-                self.last_update = now
+            self.vtk_render_window.Render()
 
 def print_cam_props(camera):
     print 'camera.SetParallelProjection',camera.GetParallelProjection()
@@ -278,20 +273,21 @@ def print_cam_props(camera):
     print
     
 def main():
-    listener = Listener()
+    # initialize GUI
+    #app = App(redirect=1,filename='viewer_log.txt')
+    app = App(redirect=0)
+    #app = App() 
+
+    listener = Listener(app)
     listener.setDaemon(True) # don't let this thread keep app alive
     listener.start()
     
-    # initialize GUI
-    #app = App(redirect=1,filename='viewer_log.txt')
-    app = App() 
-
     app.MainLoop()
 
     print_cam_props(app.cam1)
     print_cam_props(app.cam2)
     del app
-    listener.quit()
+##    listener.quit()
 
 if __name__ == '__main__':
     main()
