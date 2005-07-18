75939858ccfd31b2a7c5d86ca608e7a181444ceb
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2005-07-18 17:15:42 +0000

    Use UDP communication of realtime data by default.
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@570 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- common_variables.py
+++ common_variables.py
@@ -1,4 +1,4 @@
 MINIMUM_ECCENTRICITY = 1.4 # threshold to fit line
 ACCEPTABLE_DISTANCE_PIXELS = 10.0
-REALTIME_UDP = False # use UDP or TCP to send realtime data
-#REALTIME_UDP = True # use UDP or TCP to send realtime data
+#REALTIME_UDP = False # use UDP or TCP to send realtime data
+REALTIME_UDP = True # use UDP or TCP to send realtime data
