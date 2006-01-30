144d7ab7571ffcb4d367e273418fab624278e4ca
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-01-30 02:58:51 +0000

    latest changes
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@733 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- analysis/setup_analysis_dir.py
+++ analysis/setup_analysis_dir.py
@@ -2,5 +2,6 @@ ln -s ~/kookaburra/flydra/analysis/result_browser.py
 ln -s ~/kookaburra/flydra/analysis/PQmath.py
 ln -s ~/kookaburra/flydra/analysis/vtk_results.py
 
+ln -s ~/kookaburra/flydra/analysis/FOE_utils.py
 ln -s ~/kookaburra/flydra/analysis/analyze_FOE.py
 ln -s ~/kookaburra/flydra/analysis/plot_yvels.py
