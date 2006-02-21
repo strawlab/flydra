438378ded965d59819da0779ec4d380306898220
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-02-21 06:46:04 +0000

    updated to work with new filenames
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@858 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- analysis/setup_analysis_dir.sh
+++ analysis/setup_analysis_dir.sh
@@ -3,6 +3,8 @@ ln -s ~/kookaburra/flydra/analysis/PQmath.py
 ln -s ~/kookaburra/flydra/analysis/vtk_results.py
 
 ln -s ~/kookaburra/flydra/analysis/FOE_utils.py
-ln -s ~/kookaburra/flydra/analysis/analyze_FOE.py
-ln -s ~/kookaburra/flydra/analysis/plot_foe.py
-ln -s ~/kookaburra/flydra/analysis/plot_foe_vtk.py
+ln -s ~/kookaburra/flydra/analysis/FOE_analysis_toolchain.txt
+ln -s ~/kookaburra/flydra/analysis/foe_1_filter_triggers.py
+ln -s ~/kookaburra/flydra/analysis/foe_2_prelim_analysis.py
+ln -s ~/kookaburra/flydra/analysis/foe_3_vtk.py
+ln -s ~/kookaburra/flydra/analysis/foe_4_stats_play.py
