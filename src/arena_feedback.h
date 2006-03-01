10ec686fd9a6a7dc9e84c005c3eedda89ce892fb
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-03-01 20:11:14 +0000

    moving ArenaController into its own packages
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@900 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- src/arena_feedback.h
+++
@@ -1,7 +0,0 @@
-#ifndef _flydra_ARENA_FEEDBACK_h_
-#define _flydra_ARENA_FEEDBACK_h_
-
-void set_patt_position( double orientation, double timestamp, long framenumber,
-    double *patt_x, double *patt_y, double *out1, double *out2, double *out3 );
-
-#endif
