bb1deab37fa09e8a31071123928085e3ad667aad
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-03-05 21:53:09 +0000

    code cleanups and conversion to ATmega169
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@913 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- avrTrigger/trigger_main.c
+++ avrTrigger/trigger_main.c
@@ -1,25 +1,16 @@
 #include "handler.h"
 
 #include <avr/io.h>
-#define F_CPU 16000000
-#define HANDLER_FREQ(X) (F_CPU/256)
 
-void isr(void);
-/*void isr2(void);*/
+void isr(void); // forward definition
 
 int main(void)
 {
   Handler_Init();
 
-  DDRE=0xFF; // data direction register for port E = all output
-  DDRF=0xFF; // data direction register for port F = all output
+  DDRB=0xFF; // data direction register for port B = all output
 
-  PORTE=0x08;
-  PORTF=0;
-
-  // n/(F_CPU/256) // period in seconds
-  Reg_Handler(isr,313,0,1); // period = 5.xxx msec
-  //Reg_Handler(isr2,6250,1,1); // period = 100 msec
+  Reg_Handler(isr,31300,0,1); // period = 5.xxx msec
 
   while(1);
 
@@ -27,10 +18,5 @@ int main(void)
 }
 
 void isr(void) {
-  PORTF=~PORTF;
+  PORTB=~PORTB;
 }
-/*
-void isr2(void) {
-  PORTE=~PORTE;
-}
-*/
