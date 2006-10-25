afbf34b58b40bcc7fafdb8169267247eae7b8d9b
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-10-25 01:53:51 +0000

    convert to numpy
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1413 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- analysis/cam_params_play.py
+++ analysis/cam_params_play.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
-##import Numeric as na
-##from Numeric import *
-##from LinearAlgebra import *
-import numarray as na
-from numarray import *
-from numarray.linear_algebra import *
+import numpy
 import sys
 
 # Extract (linear) camera parameters.
@@ -14,15 +9,15 @@ def cross(a,b):
     cross.append( a[1]*b[2]-a[2]*b[1] )
     cross.append( a[2]*b[0]-a[0]*b[2] )
     cross.append( a[0]*b[1]-a[1]*b[0] )
-    return asarray(cross)
+    return numpy.asarray(cross)
 
 def norm(a):
-    return sqrt(sum(a**2))
+    return numpy.sqrt(numpy.sum(a**2))
 
 def rq(X):
-    Qt, Rt = qr_decomposition(transpose(X))
-    Rt = transpose(Rt)
-    Qt = transpose(Qt)
+    Qt, Rt = numpy.linalg.qr(numpy.transpose(X))
+    Rt = numpy.transpose(Rt)
+    Qt = numpy.transpose(Qt)
 
     Qu = []
 
@@ -34,8 +29,8 @@ def rq(X):
 
     Qu.append( cross(Qu[0], Qu[1] ) )
 
-    R = matrixmultiply( Rt, transpose(Qu))
-    Q = matrixmultiply( Qu, Qt )
+    R = numpy.dot( Rt, numpy.transpose(Qu))
+    Q = numpy.dot( Qu, Qt )
 
     return R, Q
 
@@ -45,20 +40,20 @@ def rq(X):
 # and Zisserman, example 6.2, p. 163
 
 if len( sys.argv ) < 2:
-    P = array( [[ 3.53553e2,   3.39645e2,  2.77744e2,  -1.44946e6 ],
-                [-1.03528e2,   2.33212e1,  4.59607e2,  -6.32525e5 ],
-                [ 7.07107e-1, -3.53553e-1, 6.12372e-1, -9.18559e2 ]] )
+    P = numpy.array( [[ 3.53553e2,   3.39645e2,  2.77744e2,  -1.44946e6 ],
+                      [-1.03528e2,   2.33212e1,  4.59607e2,  -6.32525e5 ],
+                      [ 7.07107e-1, -3.53553e-1, 6.12372e-1, -9.18559e2 ]] )
 else:
     def load_ascii_matrix(filename):
         fd=open(filename,mode='rb')
         buf = fd.read()
         lines = buf.split('\n')[:-1]
-        return na.array([map(float,line.split()) for line in lines])
+        return numpy.array([map(float,line.split()) for line in lines])
     P=load_ascii_matrix( sys.argv[1] )
 
-orig_determinant = determinant
+orig_determinant = numpy.linalg.det
 def determinant( A ):
-    return orig_determinant( asarray( A ) )
+    return orig_determinant( numpy.asarray( A ) )
 
 # camera center
 X = determinant( [ P[:,1], P[:,2], P[:,3] ] )
@@ -66,7 +61,7 @@ Y = -determinant( [ P[:,0], P[:,2], P[:,3] ] )
 Z = determinant( [ P[:,0], P[:,1], P[:,3] ] )
 T = -determinant( [ P[:,0], P[:,1], P[:,2] ] )
 
-C_ = transpose(array( [[ X/T, Y/T, Z/T ]] ))
+C_ = numpy.transpose(numpy.array( [[ X/T, Y/T, Z/T ]] ))
 
 M = P[:,:3]
 
@@ -74,10 +69,10 @@ M = P[:,:3]
 K,R = rq(M)
 Knorm = K/K[2,2]
 
-t = matrixmultiply( -R, C_ )
+t = numpy.dot( -R, C_ )
 
 # reconstruct P via eqn 6.8 (p. 156)
-P_ = matrixmultiply( K, concatenate( (R, t), axis=1 ) )
+P_ = numpy.dot( K, numpy.concatenate( (R, t), axis=1 ) )
 
 show_results = True
 if show_results:
