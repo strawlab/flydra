47cc55bcf50746dc92db7d14d877a47049e6517f
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-10-31 08:49:34 +0000

    remove John Bender's arena control from flydra setup.py file. Remove install_requires setuptools list
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1427 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- setup.py
+++ setup.py
@@ -8,43 +8,10 @@ version = get_svnversion_persistent('flydra/version.py',version_str)
 
 import os, glob, time, sys, StringIO
 
-# flydra stuff
-BUILD_FLYDRA_ARENA = False # test for comedilib below
-
-#if sys.platform.startswith('linux'):
-#    if (os.path.exists('/usr/local/include/comedilib.h') or
-#        os.path.exists('/usr/include/comedilib.h')):
-#        BUILD_FLYDRA_ARENA = True
-
-install_requires = ['FlyMovieFormat','cam_iface']
-
 ext_modules = []
 
 ext_modules.append(Extension(name='flydra.reconstruct_utils',
                              sources=['src/reconstruct_utils.pyx']))
-
-if BUILD_FLYDRA_ARENA:
-    # not in "flydra" package namespace
-    arena_control_extension_name='ArenaController'
-    arena_control_sources=['src/ArenaController.pyx',
-                           'src/arena_control.c',
-                           'src/arena_feedback.c',
-                           'src/arena_utils.c',
-                           'src/serial_comm/serial_comm.c',
-                           ]
-    arena_control_libraries = ['comedi','rt']
-    ext_modules.append(Extension(name=arena_control_extension_name,
-                                 sources=arena_control_sources,
-                                 libraries=arena_control_libraries,
-                                 ))
-    ext_modules.append(Extension(name='flydra.scomm',
-                                 sources=['src/serial_comm/scomm.pyx',
-                                          'src/serial_comm/serial_comm.c',
-                                          ],
-                                 ))
-
-if os.name.startswith('posix'):
-    install_requires.append('posix_sched')
 
 setup(name='flydra',
       version=version,
@@ -62,7 +29,6 @@ setup(name='flydra',
     'flydra_mainbrain = flydra.wxMainBrain:main',
     ],
     },
-      install_requires = install_requires,
       zip_safe = False, # must be false for flydra_bench
       package_data={'flydra':['flydra_server.xrc',
                               'flydra_server_art.png',
