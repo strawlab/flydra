4353ac3e12e777f748e8058d21a2af866140eaa4
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-11-30 15:42:21 +0000

    latest changes
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1473 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/analysis/hand_calibration_script.py
+++ flydra/analysis/hand_calibration_script.py
@@ -51,86 +51,89 @@ if 1:
     z = 31
 
     cam1 = [
-        [86, 458, 50,0,0],
-        [550,309,80,10,0],
-        [552,153,80,20,0],
-        [86, 155, 50, 20,0],
-        [182, 384, 60, 10, z],
-        [444, 386, 70, 10, z],
-        [449, 120, 70, 20, z],
-        [182, 123, 60, 20, z],
-        [84.9, 308, 50, 10, 0],
-        [235, 309, 60, 10, 0],
-        [393, 310, 70, 10, 0],
-        [550, 309, 80, 10, 0],
-        [85.5, 155, 50, 20, 0],
-        [237, 153, 60, 20, 0],
-        [395, 152, 70, 20, 0],
-        [552, 152, 80, 20, 0],
-        [180, 382, 60, 10, z],
-        [444, 385, 70, 10, z],
-        [181, 121, 60, 20, z],
-        [447, 118, 70, 20, z],
+        [83,288,30,10,0],
+        [192,289,40,10,0],
+        [561, 293, 70, 10, 0],
+        [562, 162, 70, 20, 0],
+        [432,419,60,0,0],
+        [80,169,30,20,0],
+        [149,340,50,10,z],
+        [514,346,70,10,z],
+        [515,151,70,20,z],
+        [146,158,50,20,z],
         ]
 
     cam2 = [
-        [275, 267, 10, 10, 0],
-        [559, 266, 50, 10, 0],
-        [644, 150, 60, 20, 0],
-        [269, 177, 10, 20, 0],
-        [545, 319, 70, 10, z],
-        [535, 151, 70, 20, z],
-        [41.4, 300, 10, 10, z],
-        [32.8, 194, 10, 20, z],
-        [326, 450, 50, 0, z],
-        [280, 350, 10, 0, 0],
+        [282,350,10,0,0],
+        [337,265,20,10,0],
+        [652,264,60,10,0],
+        [646,148,60,20,0],
+        [215,179,0,20,0],
+        [30,295,10,10,z],
+        [82,297,20,10,z],
+        [517,471,70,0,z],
+        [638,136,80,20,z],
+        [21,191,10,20,z],
         ]
 
     cam3 = [
-        [38,244, 20, 10, 0],
-        [599, 254, 80, 0, 0],
-        [605, 79.3, 80, 20, 0],
-        [17.3, 138, 20, 20, 0],
-        [338, 96.1, 50, 20, 0],
-        [143, 406, 30, 10, z],
-        [532, 484, 50, 0, z],
-        [557, 131, 50, 20, z],
-        [96.7, 185, 30, 20, z],
+        [92,296,20,10,0],
+        [190,396,30,0,0],
+        [494,392,60,0,0],
+        [593,288,70,10,0],
+        [594,188,70,20,0],
+        [93,195,20,20,0],
+        [76,318,30,10,z],
+        [607,485,60,0,z],
+        [609,130,60,20,z],
+        [252,316,40,10,z],
         ]
 
     cam4 = [
-        [231,267, 80, 10, 0],
-        [552,412, 40, 0, 0],
-        [547,162, 40, 20, 0],
-        [228, 170, 80, 20, 0],
-        [54, 434, 60, 0, z],
-        [456,356,20,10,z],
-        [450, 160, 20, 20, z],
-        [43, 172, 60, 20, z],
+        [104,414,40,0,0],
+        [424,268,80,10,0],
+        [281,388,60,0,0],
+        [427,171,80,20,0],
+        [108,164,40,20,0],
+        [199,283,50,10,0],
+        [160,360,20,10,z],
+        [587,303,60,10,z],
+        [644,416,70,0,z],
+        [593,168,60,20,z],
+        [165,154,20,20,z],
         ]
 
     cam5 = [
-        [25,373,40,10,0],
-        [517,365, 10,10,0],
-        [512,196,10,20,0],
-        [18,209,40,20,0],
-        [7,462,30,10,z],
-        [579,454,10,10,z],
-        [571,159,10,20,z],
-        [278,166,20,20,z],
-        [179,205,30,20,0],
-        [352,370,20,10,0],
+        [141,378,20,10,z],
+        [501,364,40,10,z],
+        [509,183,40,20,z],
+        [145,177,20,20,z],
+        [83,321,20,10,0],
+        [559,312,60,10,0],
+        [563,196,60,20,0],
+        [447,435,50,0,0],
+        [87,188,20,20,0],
+        [212,319,30,10,0],
         ]
 
-    cams = [(cam1,'cam1'),
-            (cam2,'cam2'),
-            (cam3,'cam3'),
-            (cam4,'cam4'),
-            (cam5,'cam5'),
+    cams = [(cam1,'cam1_0'),
+            (cam2,'cam2_0'),
+            (cam3,'cam3_0'),
+            (cam4,'cam4_0'),
+            (cam5,'cam5_0'),
             ]
 
-    for (cam,cam_name) in cams:
+    sccs = []
+
+    for (cam,cam_id) in cams:
         cam = numpy.array(cam, dtype=numpy.float)
+        if 1:
+            # on 20061121, the upper images were shifted by Y-2
+            print 'performing Y correction'
+            zcoord = cam[:,4]
+            cond = zcoord==z
+            cam[:,3] = cam[:,3]+2
+        
         X3d = cam[:,2:5]*10.0 # put in mm
         x2d = cam[:,0:2]
         B,c = build_Bc(X3d,x2d)
@@ -138,6 +141,52 @@ if 1:
         a_vec,residuals = DLT_avec_results[:2]
         Mhat = numpy.array(list(a_vec)+[1])
         Mhat.shape=(3,4)
-        print cam_name,center(Mhat).T,'residuals:',float(residuals)
-        fname = cam_name + 'DLT.txt'
+        print cam_id,center(Mhat).T,'residuals:',float(residuals)
+        fname = cam_id + 'DLT.txt'
         save_ascii_matrix(fname,Mhat)
+
+        if 0:
+            if cam_id == 'cam2_0':
+                all_rows = numpy.arange( cam.shape[0] )
+                all_X3d = cam[:,2:5]*10.0 # put in mm
+                all_x2d = cam[:,0:2]
+                
+                for test_bad_row in range(cam.shape[0]):
+                    test_good_rows = all_rows[ all_rows != test_bad_row ]
+
+                    
+                    X3d = all_X3d[test_good_rows]
+                    x2d = all_x2d[test_good_rows]
+                    B,c = build_Bc(X3d,x2d)
+                    DLT_avec_results = numpy.linalg.lstsq(B,c)
+                    a_vec,residuals = DLT_avec_results[:2]
+                    Mhat = numpy.array(list(a_vec)+[1])
+                    Mhat.shape=(3,4)
+
+                    print 'test_bad_row',test_bad_row
+                    print 'residuals',residuals
+                    print cam[test_bad_row,:]
+                    print
+                    
+        if 1:
+            import flydra.reconstruct
+
+            res = 656,491
+            pp = res[0]/2., res[1]/2.
+            print 'cam_id',cam_id
+            print 'assuming res',res
+            print 'assuming pp',pp
+            print
+            scc = flydra.reconstruct.SingleCameraCalibration(
+                cam_id=cam_id,
+                Pmat = Mhat,
+                res = res,
+                pp = pp,
+                helper = None, ## XXX should extract focal length?
+                scale_factor = 1e-3)
+            sccs.append(scc)
+            
+    if len(sccs):
+        reconstructor = flydra.reconstruct.Reconstructor(sccs)
+        reconstructor.save_to_files_in_new_directory('cal_20061121_hand')
+                
