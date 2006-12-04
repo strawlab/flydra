7532254e24e0270e73da801c248e20c3316d056a
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-12-04 02:40:22 +0000

    allow benchmark with any backend
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1485 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/flydra_bench.py
+++ flydra/flydra_bench.py
@@ -5,7 +5,7 @@ def main():
     flydra_dir=os.path.split(os.path.abspath(__file__))[0]
     flydra_file = os.path.join(flydra_dir,'flydra_camera_node.py')
 
-    env={'CAM_IFACE_DUMMY':'1',
+    env={
          'FLYDRA_BENCHMARK':'1',
          }
 
@@ -15,9 +15,6 @@ def main():
         g.update({'__name__':'__main__'})
         lc=g
         sys.argv[0] = flydra_file
-        if len(sys.argv)>1:
-            del sys.argv[1:]
-        sys.argv.extend( '--wrapper dummy --backend dummy'.split() )
         execfile(flydra_file,g,lc)
     else:
         args = [sys.executable,flydra_file]
