e9562c23bbf750f46bd57bbddd6a5454f5d71496
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-12-14 22:15:30 +0000

    automatic name finding...
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1499 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/analysis/ukine.py
+++ flydra/analysis/ukine.py
@@ -39,12 +39,56 @@ elif 0:
     # hdf5 file containing calibration data
     cal_source = 'DATA20060724_173517.h5'
     reconstructor_source = tables.openFile(cal_source,mode='r')
-elif 1:
+elif 0:
     base_fname = 'full_20060915_171705_%s_bg.fmf'
     # hdf5 file containing calibration data
     cal_source = 'DATA20060915_173304.h5'
     reconstructor_source = tables.openFile(cal_source,mode='r')
+elif 0:
+    base_fname = 'full_20061129_125343_%s_bg.fmf'
+    # hdf5 file containing calibration data
+    cal_source = 'DATA20061127_193406.h5'
+    reconstructor_source = tables.openFile(cal_source,mode='r')
+elif 0:
+    base_fname = 'full_20061205_193159_%s_bg.fmf'
+    # hdf5 file containing calibration data
+    cal_source = 'DATA20061205_193629.h5'
+    reconstructor_source = tables.openFile(cal_source,mode='r')
+else:
+    files = os.listdir(os.curdir)
+    fmfs = [f for f in files if f.startswith('full') and f.endswith('bg.fmf')]
+    cals = [f for f in files if f.startswith('DATA') and f.endswith('.h5')]
 
+    use_fmf_names = []
+    cam_ids = []
+    for fmf in fmfs:
+        fmf = fmf.replace('_bg.fmf','')
+        spl = fmf.split('_')
+        start = '_'.join(spl[:3])
+        cam_id = '_'.join(spl[3:])
+        if start not in use_fmf_names:
+            use_fmf_names.append( start )
+        if cam_id not in cam_ids:
+            cam_ids.append( cam_id )
+        
+    print fmfs
+    print cals
+
+    print use_fmf_names
+    print 'cam_ids',cam_ids
+
+    if len(use_fmf_names)>1:
+        print use_fmf_names
+        raise ValueError('too many fmf names')
+
+    if len(cals)>1:
+        print cals
+        raise ValueError('too many calibration files')
+    
+    base_fname = use_fmf_names[0] +'_%s_bg.fmf'
+    # hdf5 file containing calibration data
+    cal_source = cals[0]
+    reconstructor_source = tables.openFile(cal_source,mode='r')
 
 recon = flydra.reconstruct.Reconstructor(reconstructor_source)
 
