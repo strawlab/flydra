ddee9f94479e6ee388a71b083a533031df137838
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-12-29 23:34:58 +0000

    make print_camera_summary a command-line command
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1521 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ flydra/analysis/flydra_analysis_print_camera_summary.py
@@ -0,0 +1,40 @@
+import result_utils
+import sys
+from optparse import OptionParser
+
+def main():
+    usage = '%prog FILE [options]'
+    
+    parser = OptionParser(usage)
+
+    parser.add_option("--writeable", action='store_true',dest='writeable',
+                      help="open file in writeable mode")
+
+    (options, args) = parser.parse_args()
+    
+    if len(args)>1:
+        print >> sys.stderr,  "arguments interpreted as FILE supplied more than once"
+        parser.print_help()
+        return
+    
+    if len(args)<1:
+        parser.print_help()
+        return
+
+    h5_filename=args[0]
+    if options.writeable:
+        mode = 'r+'
+    else:
+        mode = 'r'
+
+    results = result_utils.get_results( h5_filename, mode=mode)
+    if not hasattr(results.root,'data2d_camera_summary'):
+        print >> sys.stderr, "ERROR: no table 'data2d_camera_summary' (try opening in writeable mode)"
+        sys.exit(1)
+
+    print results.root.data2d_camera_summary.colnames    
+    for row in results.root.data2d_camera_summary:
+        print row
+
+if __name__=='__main__':
+    main()
