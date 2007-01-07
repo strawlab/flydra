5f1947134d54214a7d02e06677ff0378e8814bc0
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2007-01-07 08:36:55 +0000

    update dynamic model hbird2
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1536 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/kalman/dynamic_models.py
+++ flydra/kalman/dynamic_models.py
@@ -11,18 +11,23 @@ def get_dynamic_model_dict():
     # process covariance
     Q = numpy.zeros((ss,ss))
     for i in range(6,9):
-        Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
+        #Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
+        Q[i,i] = 50.0
 
     # measurement noise covariance matrix
     #R = 1e-6*numpy.eye(os) # (1mm)**2 = (0.001m)**2
-    R = 1e-4*numpy.eye(os) # (10mm)**2 = (0.01m)**2
+    #R = 1e-4*numpy.eye(os) # (10mm)**2 = (0.01m)**2
+    R = 8e-4*numpy.eye(os)
+    #R = 2.5e-3*numpy.eye(os) # (50mm)**2 = (0.05m)**2
 
     dynamic_models['hbird2, units: mm'] = dict(
         scale_factor=1e-3,
-        n_sigma_accept=5.0,
-        max_variance_dist_meters=0.050, # allow error to grow to 50 mm before dropping
-        initial_position_covariance_estimate=1e-6, #1mm ( (1e-3)**2 meters)
-        initial_acceleration_covariance_estimate=15,
+        #n_sigma_accept=2.8,
+        n_sigma_accept=2.4,
+        max_variance_dist_meters=0.040, # allow error to grow to 40 mm before dropping
+        initial_position_covariance_estimate=1e-4, #10mm ( (1e-2)**2 meters)
+        #initial_acceleration_covariance_estimate=15,
+        initial_acceleration_covariance_estimate=10,
         Q=Q,
         R=R)
     
