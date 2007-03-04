5d7a2abe8057ea55ad6ae947937f2c0cea711ce1
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2007-03-04 02:00:40 +0000

    add utility to perform Kalman smoothing as save as .mat file
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1626 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ flydra/a2/data2smoothed.py
@@ -0,0 +1,48 @@
+import numpy
+import sys, os
+import detect_saccades
+from optparse import OptionParser
+import flydra.analysis.flydra_analysis_convert_to_mat
+
+def convert(infilename,outfilename,frames_per_second=100.0):
+    ca = detect_saccades.CachingAnalyzer()
+    obj_ids = ca.get_obj_ids(infilename)
+    allrows = []
+    for i,obj_id in enumerate(obj_ids):
+        if i%100 == 0:
+            print '%d of %d'%(i,len(obj_ids))
+        results = ca.calculate_trajectory_metrics(obj_id,
+                                                  infilename,
+                                                  use_kalman_smoothing=True,
+                                                  frames_per_second=frames_per_second)
+        rows = results['kalman_smoothed_rows']
+        allrows.append(rows)
+    allrows = numpy.concatenate( allrows )
+    recarray = numpy.rec.array(allrows)
+    flydra.analysis.flydra_analysis_convert_to_mat.do_it(
+        rows=recarray,
+        ignore_observations=True,
+        newfilename=outfilename)
+        
+
+def main():
+    usage = '%prog FILE [options]'
+    
+    parser = OptionParser(usage)
+    (options, args) = parser.parse_args()
+    
+    if len(args)>1:
+        print >> sys.stderr,  "arguments interpreted as FILE supplied more than once"
+        parser.print_help()
+        return
+    
+    if len(args)<1:
+        parser.print_help()
+        return
+
+    infilename = args[0]
+    outfilename = os.path.splitext(infilename)[0] + '_smoothed.mat'
+    convert(infilename,outfilename)
+    
+if __name__=='__main__':
+    main()
