b5fd7890a088e8a3772c749f495636e91b1105e2
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2007-03-05 22:11:26 +0000

    use more sophisticated distance-finding code
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1630 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/a2/find_closet_approaches.py
+++ flydra/a2/find_closet_approaches.py
@@ -4,7 +4,112 @@ import stimulus_positions
 from conditions import files, condition_names, stim_names
 import tables
 import numpy
-import pickle
+import scipy.io
+import pickle, os
+
+
+def calc_dist_old(lineseg,verts):
+    xavg = (lineseg[0][0] + lineseg[1][0])/2
+    yavg = (lineseg[0][1] + lineseg[1][1])/2
+
+    xdist = verts[:,0]-xavg
+    ydist = verts[:,1]-yavg
+
+    dist = numpy.sqrt(xdist**2+ydist**2)
+    return dist
+
+def calc_dist(lineseg,verts):
+    """return minimum distance between lineseg and vertices
+
+In threespace (ascii art):
+
+ u and v vectors are vertices of lineseg.
+ w vector(s) are verts.
+
+then:
+
+ a = v-u
+ b = w-u
+
+we can find:
+
+ x = (projection of b onto a) + u
+ 
+ x is the projection
+ 
+   * v
+   |
+   |
+   |
+   |
+ x *   * w
+   |  /
+   | /
+   |/
+   * u
+
+
+    """
+    def vec_abs(x):
+        return numpy.sqrt(numpy.sum( x**2, axis=0 ))
+    v = numpy.asarray(lineseg[0])
+    u = numpy.asarray(lineseg[1])
+
+    w = numpy.asarray(verts).T
+    n_pts = w.shape[1]
+
+    a = v-u
+    b = w-u[:,numpy.newaxis]
+
+    amag = vec_abs(a)
+    anorm = a/amag
+    
+    len_b_projected_on_a = numpy.dot(anorm,b)
+
+    # closest point on axis of lineseg:
+    x = len_b_projected_on_a[numpy.newaxis,:]*anorm[:,numpy.newaxis] + u[:,numpy.newaxis]
+    perpendicular_dist = vec_abs(w-x)
+
+    u_dist = vec_abs(w-u[:,numpy.newaxis])
+    v_dist = vec_abs(w-v[:,numpy.newaxis])
+    endpoint_dist = numpy.where( u_dist < v_dist, u_dist, v_dist )
+
+    midpoint_closest_cond = (len_b_projected_on_a <= amag) & (len_b_projected_on_a >= 0)
+    dist = numpy.where( midpoint_closest_cond, perpendicular_dist, endpoint_dist )
+
+    if 0:
+        print 'u',u
+        print 'v',v
+        print
+        print 'w[:,:4]'
+        print w[:,:4]
+        print
+        print 'len_b_projected_on_a[:4]'
+        print len_b_projected_on_a[:4]
+        print
+        print 'a'
+        print a
+        print
+        print 'len_b_projected_on_a[numpy.newaxis,:]*anorm[:,numpy.newaxis]'
+        print (len_b_projected_on_a[numpy.newaxis,:]*anorm[:,numpy.newaxis])[:,:4]
+        print
+        print 'x[:,:4]'
+        print x[:,:4]
+        print
+        print 'u_dist[:4]'
+        print u_dist[:4]
+        print
+        print 'v_dist[:4]'
+        print v_dist[:4]
+        print
+        print 'perpendicular_dist[:4]'
+        print perpendicular_dist[:4]
+        print
+        print 'dist[:4]'
+        print dist[:4]
+        print
+        1/0
+    return dist
 
 if 1:
     must_have_been_dist_away = 0.05 # 50 mm
@@ -17,35 +122,44 @@ if 1:
     by_filename = {}
     ca = detect_saccades.CachingAnalyzer()
     #for condition_name in condition_names:
-    for condition_name in ['d2']:
+    for condition_name in ['half no odor, w/ wind']:
         filenames = files[condition_name]
         stim = stim_names[condition_name]
         all_stim_verts = stimulus_positions.stim_positions[stim]
 
         for filename in filenames:
             print 'filename',filename,condition_name
-            kresults = tables.openFile(filename,mode='r')
-            obj_ids = kresults.root.kalman_estimates.read(field='obj_id',flavor='numpy')
-            use_obj_ids = numpy.unique(obj_ids)
+
+            if os.path.splitext(filename)[1] == '.mat':
+                mat_data = scipy.io.mio.loadmat(filename)
+                obj_ids = mat_data['kalman_obj_id']
+                obj_ids = obj_ids.astype( numpy.uint32 )
+                obs_obj_ids = obj_ids # use as observation length, even though these aren't observations
+                use_obj_ids = numpy.unique(obj_ids)
+                is_mat_file = True
+                data_file = mat_data
+            else:
+                kresults = tables.openFile(filename,mode='r')
+                obj_ids = kresults.root.kalman_estimates.read(field='obj_id',flavor='numpy')
+                use_obj_ids = numpy.unique(obj_ids)
+                is_mat_file = False
+                data_file = kresults
 
             min_dist_by_obj_id = {}
             min_dists = []
             use_idxs = []
             for obj_id_enum,obj_id in enumerate(use_obj_ids):
-##                if obj_id>1000:
-##                    break
-                
                 if obj_id_enum%100 == 0:
                     print 'object %d of %d'%(obj_id_enum,len(use_obj_ids))
-                    
                 verts = ca.get_raw_positions(obj_id,
-                                             kresults)
+                                             data_file)
                 if len(verts)<20:
                     continue
 
-                z = verts[:,2]
-                narrow_z_cond = (z >= 0.05) & (z <= 0.25)
-                verts = verts[narrow_z_cond]
+                if 0:
+                    z = verts[:,2]
+                    narrow_z_cond = (z >= 0.05) & (z <= 0.25)
+                    verts = verts[narrow_z_cond]
 
                 if len(verts) == 0:
                     continue
@@ -53,14 +167,8 @@ if 1:
                 mindist = numpy.inf
                 maxdist = 0
                 for lineseg in all_stim_verts:
-                    
-                    xavg = (lineseg[0][0] + lineseg[1][0])/2
-                    yavg = (lineseg[0][1] + lineseg[1][1])/2
-
-                    xdist = verts[:,0]-xavg
-                    ydist = verts[:,1]-yavg
-
-                    dist = numpy.sqrt(xdist**2+ydist**2)
+                    #dist = calc_dist_old(lineseg,verts)
+                    dist = calc_dist(lineseg,verts)
                     if dist.max() < must_have_been_dist_away:
                         continue
 
@@ -91,7 +199,8 @@ if 1:
             by_filename[filename] = closest_flights_obj_ids
 
     ca.close()
-    kresults.close()
+    if not is_mat_file:
+        kresults.close()
     
     fd = open('closest.pkl',mode='wb')
     pickle.dump(by_filename,fd)
