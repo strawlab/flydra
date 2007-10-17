f20ecf8bd1dfd76769eb3728b4f7da526c710fc5
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2007-10-17 00:27:51 +0000

    add script
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1806 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ at90usb_trigger/firmware/flash_device.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+flydra_trigger_enter_dfu_mode && \
+sleep 3 && \
+sudo dfu-programmer at90usb1287 erase && \
+sudo dfu-programmer at90usb1287 flash trigger_control.hex && \
+sudo dfu-programmer at90usb1287 start
+
