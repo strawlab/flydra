a80b602e8018a858badb967ad77ce1d41af0c5ec
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2007-10-24 22:17:49 +0000

    trigger works with Photron APX camera
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1825 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- at90usb_trigger/firmware/trigger_task.c
+++ at90usb_trigger/firmware/trigger_task.c
@@ -191,7 +191,7 @@ void init_pwm_output(void) {
   */
 
   // set output direction on pin
-  PORTC &= 0x8F; // pin C4-6 set low to start
+  PORTC &= 0x87; // pin C3-6 set low to start
   DDRC |= 0x7F; // enable output for:
   // (Output Compare and PWM) A-C of Timer/Counter 3
   // PORTC 3 (EXT_TRIG1)
@@ -434,11 +434,12 @@ void trigger_task(void)
       }
 
       if (flags & TASK_FLAGS_SET_EXT_TRIG1) {
-	PORTC &= 0xF7; // clear bit
-	for (i=0;i<255;i++) {
+	PORTC |= 0x08; // raise bit
+	for (i=0;i<10;i++) {
+	  tmp++;
 	  // do nothing
 	}
-	PORTC |= 0x08; // raise bit
+	PORTC &= 0xF7; // clear bit
 
 	/*
 	framecount_EXT_TRIG1  = get_framecount_A();
