c840f44e9e9248fadcc01380074c144aa53eaff0
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2007-11-09 03:30:38 +0000

    ignore firmware version when flashing
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@1863 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- at90usb_trigger/firmware/flash_device.sh
+++ at90usb_trigger/firmware/flash_device.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-flydra_trigger_enter_dfu_mode && \
+flydra_trigger_enter_dfu_mode --ignore-version-mismatch && \
 sleep 3 && \
 sudo dfu-programmer at90usb1287 erase && \
 sudo dfu-programmer at90usb1287 flash trigger_control.hex && \
