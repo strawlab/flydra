ffc79ace30643c4ef6fd3bf628510f5f67119876
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-02-01 03:43:32 +0000

    allow saving of points without 3D Kalman data
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2049 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/analysis/flydra_analysis_generate_recalibration.py
+++ flydra/analysis/flydra_analysis_generate_recalibration.py
@@ -23,10 +23,38 @@ def save_ascii_matrix(thefile,m):
         fd.write( ' '.join(map(str,row)) )
         fd.write( '\n' )
 
+def create_new_row(d2d, this_camns, this_camn_idxs, cam_ids, camn2cam_id, npoints_by_cam_id):
+    n_pts = 0
+    IdMat_row = []
+    points_row = []
+    for cam_id in cam_ids:
+        found = False
+        for this_camn,this_camn_idx in zip(this_camns,this_camn_idxs):
+            if camn2cam_id[this_camn] != cam_id:
+                continue
+            npoints_by_cam_id[cam_id] = npoints_by_cam_id[cam_id] + 1
+
+            this_camn_d2d = d2d[d2d['camn'] == this_camn]
+            for this_row in this_camn_d2d: # XXX could be sped up
+                if this_row['frame_pt_idx'] == this_camn_idx:
+                    found = True
+                    break
+        if not found:
+            IdMat_row.append( 0 )
+            points_row.extend( [numpy.nan, numpy.nan, numpy.nan] )
+        else:
+            n_pts += 1
+            IdMat_row.append( 1 )
+            points_row.extend( [this_row['x'], this_row['y'], 1.0] )
+    return IdMat_row, points_row
+
 def do_it(filename,
           efilename,
           use_nth_observation=40,
           h5_2d_data_filename=None,
+          use_kalman_data=True,
+          start=None,
+          stop=None,
           ):
 
     if h5_2d_data_filename is None:
@@ -36,33 +64,33 @@ def do_it(filename,
     if not os.path.exists(calib_dir):
         os.makedirs(calib_dir)
 
-    mylocals = {}
-    myglobals = {}
-    execfile(efilename,myglobals,mylocals)
-
-    use_obj_ids = mylocals['long_ids']
-    if 'bad' in mylocals:
-        use_obj_ids = sets.Set(use_obj_ids)
-        bad = sets.Set(mylocals['bad'])
-        use_obj_ids = list(use_obj_ids.difference(bad))
-
     results = result_utils.get_results(filename,mode='r+')
-    h5_2d_data = result_utils.get_results(h5_2d_data_filename,mode='r+')
+
+    if use_kalman_data:
+        mylocals = {}
+        myglobals = {}
+        execfile(efilename,myglobals,mylocals)
+
+        use_obj_ids = mylocals['long_ids']
+        if 'bad' in mylocals:
+            use_obj_ids = sets.Set(use_obj_ids)
+            bad = sets.Set(mylocals['bad'])
+            use_obj_ids = list(use_obj_ids.difference(bad))
+        kobs = results.root.kalman_observations
+        kobs_2d = results.root.kalman_observations_2d_idxs
     reconstructor = flydra.reconstruct.Reconstructor(results)
+
+    h5_2d_data = result_utils.get_results(h5_2d_data_filename,mode='r+')
 
     camn2cam_id, cam_id2camns = result_utils.get_caminfo_dicts(h5_2d_data)
 
     cam_ids = cam_id2camns.keys()
     cam_ids.sort()
 
-    kobs = results.root.kalman_observations
-
     data2d = h5_2d_data.root.data2d_distorted
     #use_idxs = numpy.arange(data2d.nrows)
     frames = data2d.cols.frame[:]
     qfi = result_utils.QuickFrameIndexer(frames)
-
-    kobs_2d = results.root.kalman_observations_2d_idxs
 
     npoints_by_cam_id = {}
     for cam_id in cam_ids:
@@ -70,98 +98,104 @@ def do_it(filename,
 
     IdMat = []
     points = []
-    for obj_id_enum, obj_id in enumerate(use_obj_ids):
-        print 'obj_id %d (%d of %d)'%(obj_id, obj_id_enum+1, len(use_obj_ids))
-        this_obj_id = obj_id
-        k_use_idxs = kobs.getWhereList(
-            'obj_id==this_obj_id')
-        obs_2d_idxs = kobs.readCoordinates( k_use_idxs,
-                                            field='obs_2d_idx')
-        kframes = kobs.readCoordinates( k_use_idxs,
-                                        field='frame')
-        kframes_use = kframes[::use_nth_observation]
-        obs_2d_idxs_use = obs_2d_idxs[::use_nth_observation]
 
-        for n_kframe, (kframe, obs_2d_idx) in enumerate(zip(kframes_use,obs_2d_idxs_use)):
-            #print
-            print 'kframe %d (%d of %d)'%(kframe,n_kframe+1,len(kframes_use))
-            if 0:
-                k_use_idx = k_use_idxs[n_kframe*use_nth_observation]
-                print kobs.readCoordinates( numpy.array([k_use_idx]))
-            if PT.__version__ <= '1.3.3':
-                obs_2d_idx_find = int(obs_2d_idx)
-                kframe_find = int(kframe)
-            else:
-                obs_2d_idx_find = obs_2d_idx
-                kframe_find = kframe
-            obj_id_save = int(obj_id) # convert from possible numpy scalar
+    if use_kalman_data:
+        for obj_id_enum, obj_id in enumerate(use_obj_ids):
+            print 'obj_id %d (%d of %d)'%(obj_id, obj_id_enum+1, len(use_obj_ids))
+            this_obj_id = obj_id
+            k_use_idxs = kobs.getWhereList(
+                'obj_id==this_obj_id')
+            obs_2d_idxs = kobs.readCoordinates( k_use_idxs,
+                                                field='obs_2d_idx')
+            kframes = kobs.readCoordinates( k_use_idxs,
+                                            field='frame')
+            kframes_use = kframes[::use_nth_observation]
+            obs_2d_idxs_use = obs_2d_idxs[::use_nth_observation]
 
-            #sys.stdout.write('  reading frame data...')
-            #sys.stdout.flush()
-            obs_2d_idx_find_next = obs_2d_idx_find+numpy.uint64(1)
-            kobs_2d_data = kobs_2d.read( start=obs_2d_idx_find,
-                                         stop=obs_2d_idx_find_next )
-            #sys.stdout.write('done\n')
-            #sys.stdout.flush()
+            for n_kframe, (kframe, obs_2d_idx) in enumerate(zip(kframes_use,obs_2d_idxs_use)):
+                #print
+                print 'kframe %d (%d of %d)'%(kframe,n_kframe+1,len(kframes_use))
+                if 0:
+                    k_use_idx = k_use_idxs[n_kframe*use_nth_observation]
+                    print kobs.readCoordinates( numpy.array([k_use_idx]))
+                if PT.__version__ <= '1.3.3':
+                    obs_2d_idx_find = int(obs_2d_idx)
+                    kframe_find = int(kframe)
+                else:
+                    obs_2d_idx_find = obs_2d_idx
+                    kframe_find = kframe
+                obj_id_save = int(obj_id) # convert from possible numpy scalar
 
-            assert len(kobs_2d_data)==1
-            kobs_2d_data = kobs_2d_data[0]
-            this_camns = kobs_2d_data[0::2]
-            this_camn_idxs = kobs_2d_data[1::2]
+                #sys.stdout.write('  reading frame data...')
+                #sys.stdout.flush()
+                obs_2d_idx_find_next = obs_2d_idx_find+numpy.uint64(1)
+                kobs_2d_data = kobs_2d.read( start=obs_2d_idx_find,
+                                             stop=obs_2d_idx_find_next )
+                #sys.stdout.write('done\n')
+                #sys.stdout.flush()
 
-            #sys.stdout.write('  doing frame selections...')
-            #sys.stdout.flush()
-            if 1:
-                this_use_idxs=qfi.get_frame_idxs(kframe_find)
-            elif 0:
-                this_use_idxs=numpy.nonzero(frames==kframe_find)[0]
-            else:
-                this_use_idxs = data2d.getWhereList( 'frame==kframe_find')
-            #sys.stdout.write('done\n')
-            #sys.stdout.flush()
+                assert len(kobs_2d_data)==1
+                kobs_2d_data = kobs_2d_data[0]
+                this_camns = kobs_2d_data[0::2]
+                this_camn_idxs = kobs_2d_data[1::2]
 
-            if PT.__version__ <= '1.3.3':
-                this_use_idxs = [int(t) for t in this_use_idxs]
+                #sys.stdout.write('  doing frame selections...')
+                #sys.stdout.flush()
+                if 1:
+                    this_use_idxs=qfi.get_frame_idxs(kframe_find)
+                elif 0:
+                    this_use_idxs=numpy.nonzero(frames==kframe_find)[0]
+                else:
+                    this_use_idxs = data2d.getWhereList( 'frame==kframe_find')
+                #sys.stdout.write('done\n')
+                #sys.stdout.flush()
+
+                if PT.__version__ <= '1.3.3':
+                    this_use_idxs = [int(t) for t in this_use_idxs]
+
+                d2d = data2d.readCoordinates( this_use_idxs )
+                if len(this_camns) < 3:
+                    # not enough points to contribute to calibration
+                    continue
+                IdMat_row, points_row = create_new_row( d2d, this_camns, this_camn_idxs, cam_ids, camn2cam_id, npoints_by_cam_id )
+
+                IdMat.append( IdMat_row )
+                points.append( points_row )
+            print 'running total of points','-'*20
+            for cam_id in cam_ids:
+                print 'cam_id %s: %d points'%(cam_id,npoints_by_cam_id[cam_id])
+            print
+
+    if start is not None or stop is not None:
+        if start is None:
+            start = 0
+        if stop is None:
+            stop = frames.max()
+
+        count = 0
+        for frameno in range(start,stop+1,use_nth_observation):
+            this_use_idxs=qfi.get_frame_idxs(frameno)
 
             d2d = data2d.readCoordinates( this_use_idxs )
-            #print 'd2d',d2d
+            d2d = d2d[ ~numpy.isnan(d2d['x']) ]
+            this_camns = d2d['camn']
+
+            unique_camns = numpy.unique(this_camns)
+            if len(this_camns) != len(unique_camns):
+                # ambiguity - a camera has > 1 point
+                continue
+            this_camn_idxs = numpy.array([0]*len(this_camns))
+
             if len(this_camns) < 3:
                 # not enough points to contribute to calibration
                 continue
 
-            n_pts = 0
-            IdMat_row = []
-            points_row = []
-            for cam_id in cam_ids:
-                found = False
-                #print '    cam_id',cam_id
-                for this_camn,this_camn_idx in zip(this_camns,this_camn_idxs):
-                    #print '       %d %s'%(this_camn,camn2cam_id[this_camn])
-                    if camn2cam_id[this_camn] != cam_id:
-                        continue
-                    npoints_by_cam_id[cam_id] = npoints_by_cam_id[cam_id] + 1
+            count +=1
 
-                    this_camn_d2d = d2d[d2d['camn'] == this_camn]
-                    #print '    this_camn_d2d',this_camn_d2d
-                    for this_row in this_camn_d2d: # XXX could be sped up
-                        if this_row['frame_pt_idx'] == this_camn_idx:
-                            found = True
-                            break
-                if not found:
-                    IdMat_row.append( 0 )
-                    points_row.extend( [numpy.nan, numpy.nan, numpy.nan] )
-                else:
-                    n_pts += 1
-                    IdMat_row.append( 1 )
-                    points_row.extend( [this_row['x'], this_row['y'], 1.0] )
-            #print 'IdMat_row',IdMat_row
-            #print 'points_row',points_row
+            IdMat_row, points_row = create_new_row( d2d, this_camns, this_camn_idxs, cam_ids, camn2cam_id, npoints_by_cam_id )
             IdMat.append( IdMat_row )
             points.append( points_row )
-        print 'running total of points','-'*20
-        for cam_id in cam_ids:
-            print 'cam_id %s: %d points'%(cam_id,npoints_by_cam_id[cam_id])
-        print
+    print '%d points'%len(IdMat)
 
     IdMat = numpy.array(IdMat,dtype=numpy.uint8).T
     points = numpy.array(points,dtype=numpy.float32).T
@@ -212,7 +246,7 @@ The form of the efile is::
 
 Then run this program::
 
-  flydra_analysis_generate_recalibration <DATAfilename.h5> <efile> [options]
+  flydra_analysis_generate_recalibration <DATAfilename.h5> [EFILENAME] [options]
 
 """
 
@@ -225,6 +259,19 @@ Then run this program::
     parser.add_option('--2d-data', type='string',
                       dest='h5_2d_data_filename', default=None)
 
+    parser.add_option("--disable-kalman-objs", action='store_false', default=True,
+                      dest='use_kalman_data')
+
+    parser.add_option("--start", dest='start',
+                      type="int",
+                      help="first frame",
+                      metavar="START")
+
+    parser.add_option("--stop", dest='stop',
+                      type="int",
+                      help="last frame",
+                      metavar="STOP")
+
     (options, args) = parser.parse_args()
 
     if len(args)>2:
@@ -232,16 +279,24 @@ Then run this program::
         parser.print_help()
         return
 
-    if len(args)<2:
+    if len(args)<1:
         parser.print_help()
         return
 
     h5_filename=args[0]
-    efilename = args[1]
+    if len(args)==2:
+        efilename = args[1]
+    else:
+        efilename = None
+        if options.use_kalman_data is not False:
+            raise ValueError('Kalman objects have not been disabled, but you did not specify an EFILE (hint: specify an EFILE or use --disable-kalman-objs')
 
     do_it(h5_filename,efilename,
           use_nth_observation=options.use_nth_observation,
           h5_2d_data_filename=options.h5_2d_data_filename,
+          use_kalman_data=options.use_kalman_data,
+          start=options.start,
+          stop=options.stop,
           )
 
 if __name__=='__main__':
