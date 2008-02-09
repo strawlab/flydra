70960fd0659c443b2922bbd2b1d4185f54bd3f91
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-02-09 17:42:16 +0000

    remove models that haven't been used in a long time
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2060 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/kalman/dynamic_models.py
+++ flydra/kalman/dynamic_models.py
@@ -68,30 +68,31 @@ def create_dynamic_model_dict(dt=None):
 
     dynamic_models = {}
 
-    # 'hbird2, units: mm':
-    # process covariance
-    Q = numpy.zeros((ss,ss))
-    for i in range(6,9):
-        #Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
-        Q[i,i] = 50.0
+    ## # 'hbird2, units: mm':
+    ## # process covariance
+    ## Q = numpy.zeros((ss,ss))
+    ## for i in range(6,9):
+    ##     #Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
+    ##     Q[i,i] = 50.0
 
-    # measurement noise covariance matrix
-    #R = 1e-6*numpy.eye(os) # (1mm)**2 = (0.001m)**2
-    #R = 1e-4*numpy.eye(os) # (10mm)**2 = (0.01m)**2
-    R = 8e-4*numpy.eye(os)
-    #R = 2.5e-3*numpy.eye(os) # (50mm)**2 = (0.05m)**2
+    ## # measurement noise covariance matrix
+    ## #R = 1e-6*numpy.eye(os) # (1mm)**2 = (0.001m)**2
+    ## #R = 1e-4*numpy.eye(os) # (10mm)**2 = (0.01m)**2
+    ## R = 8e-4*numpy.eye(os)
+    ## #R = 2.5e-3*numpy.eye(os) # (50mm)**2 = (0.05m)**2
 
-    newdict = dict(
-        #n_sigma_accept=2.8,
-        n_sigma_accept=2.4,
-        max_variance_dist_meters=0.040, # allow error to grow to 40 mm before dropping
-        initial_position_covariance_estimate=1e-4, #10mm ( (1e-2)**2 meters)
-        #initial_acceleration_covariance_estimate=15,
-        initial_acceleration_covariance_estimate=10,
-        Q=Q,
-        R=R)
-    newdict.update(base_model_dict)
-    dynamic_models['hbird2, units: mm'] = newdict
+    ## newdict = dict(
+    ##     #n_sigma_accept=2.8,
+    ##     n_sigma_accept=2.4,
+    ##     max_variance_dist_meters=0.040, # allow error to grow to 40 mm before dropping
+    ##     initial_position_covariance_estimate=1e-4, #10mm ( (1e-2)**2 meters)
+    ##     #initial_acceleration_covariance_estimate=15,
+    ##     initial_acceleration_covariance_estimate=10,
+    ##     Q=Q,
+    ##     R=R)
+    ## newdict.update(base_model_dict)
+    ## dynamic_models['hbird2, units: mm'] = newdict
+
     ######################################
     # 'hbird3, units: mm':
     # process covariance
@@ -131,59 +132,67 @@ def create_dynamic_model_dict(dt=None):
     # 'fly dynamics, high precision calibration, units: mm':
     # process covariance
     Q = numpy.zeros((ss,ss))
+    for i in range(0,3):
+        Q[i,i] = (0.001)**2
+
+    for i in range(3,6):
+        Q[i,i] = (0.2)**2
+
     for i in range(6,9):
         Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
 
     # measurement noise covariance matrix
-    R = 1e-6*numpy.eye(os) # (1mm)**2 = (0.001m)**2
+    R = 1e-6*numpy.eye(os)
 
     newdict = dict(
         n_sigma_accept=3.0,
-        max_variance_dist_meters=1e-4,
-        initial_position_covariance_estimate=1e-6, #1mm ( (1e-3)**2 meters)
-        initial_acceleration_covariance_estimate=15,
+        max_variance_dist_meters=1e-2, # 10mm
+        initial_position_covariance_estimate=1e-4,
+        initial_acceleration_covariance_estimate=20,
         Q=Q,
         R=R)
     newdict.update(base_model_dict)
     dynamic_models['fly dynamics, high precision calibration, units: mm'] = newdict
 
-    # 'fly dynamics, low precision calibration, units: mm':
-    # process covariance
-    Q = numpy.zeros((ss,ss))
-    for i in range(6,9):
-        Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
+    ## ##################################################
 
-    # measurement noise covariance matrix
-    R = 1e-4*numpy.eye(os) # (10mm)**2 = (0.01m)**2
+    ## # 'fly dynamics, low precision calibration, units: mm':
+    ## # process covariance
+    ## Q = numpy.zeros((ss,ss))
+    ## for i in range(6,9):
+    ##     Q[i,i] = 10.0 # acceleration noise (near (3.16m*sec**-2)**2)
 
-    newdict = dict(
-        n_sigma_accept=3.0,
-        max_variance_dist_meters=1e-3,
-        initial_position_covariance_estimate=1e-4, #10mm ( (1e-2)**2 meters)
-        initial_acceleration_covariance_estimate=15,
-        Q=Q,
-        R=R)
-    newdict.update(base_model_dict)
-    dynamic_models['fly dynamics, low precision calibration, units: mm'] = newdict
+    ## # measurement noise covariance matrix
+    ## R = 1e-4*numpy.eye(os) # (10mm)**2 = (0.01m)**2
 
-    # 'hummingbird dynamics, units: mm'
+    ## newdict = dict(
+    ##     n_sigma_accept=3.0,
+    ##     max_variance_dist_meters=1e-3,
+    ##     initial_position_covariance_estimate=1e-4, #10mm ( (1e-2)**2 meters)
+    ##     initial_acceleration_covariance_estimate=15,
+    ##     Q=Q,
+    ##     R=R)
+    ## newdict.update(base_model_dict)
+    ## dynamic_models['fly dynamics, low precision calibration, units: mm'] = newdict
 
-    # process covariance
-    Q = numpy.zeros((ss,ss))
-    for i in range(6,9):
-        Q[i,i] = 100.0 # acceleration noise (near (10*sec**-2)**2)
+    ## # 'hummingbird dynamics, units: mm'
 
-    # measurement noise covariance matrix
-    R = 0.0025*numpy.eye(os) # (5cm)**2 = (0.05m)**2
+    ## # process covariance
+    ## Q = numpy.zeros((ss,ss))
+    ## for i in range(6,9):
+    ##     Q[i,i] = 100.0 # acceleration noise (near (10*sec**-2)**2)
 
-    newdict = dict(
-        n_sigma_accept=5.0, # XXX should reduce later?
-        max_variance_dist_meters=0.050, # allow error to grow to 50 mm before dropping
-        initial_position_covariance_estimate=0.0025, #50mm ( (5e-2)**2 meters)
-        initial_acceleration_covariance_estimate=15,
-        Q=Q,
-        R=R)
-    newdict.update(base_model_dict)
-    dynamic_models['hummingbird dynamics, units: mm'] = newdict
+    ## # measurement noise covariance matrix
+    ## R = 0.0025*numpy.eye(os) # (5cm)**2 = (0.05m)**2
+
+    ## newdict = dict(
+    ##     n_sigma_accept=5.0, # XXX should reduce later?
+    ##     max_variance_dist_meters=0.050, # allow error to grow to 50 mm before dropping
+    ##     initial_position_covariance_estimate=0.0025, #50mm ( (5e-2)**2 meters)
+    ##     initial_acceleration_covariance_estimate=15,
+    ##     Q=Q,
+    ##     R=R)
+    ## newdict.update(base_model_dict)
+    ## dynamic_models['hummingbird dynamics, units: mm'] = newdict
 
     return dynamic_models
