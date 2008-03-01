b94e75512671d9ab0dead5b5c2b6f0a19052cf66
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-03-01 17:20:45 +0000

    initial commit of relay UDP data script
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2134 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ flydra/realtime_trigger_xmit.py
@@ -0,0 +1,41 @@
+import socket
+import common_variables
+import flydra.kalman.flydra_tracker as flydra_tracker
+import numpy
+import time, math, sys
+
+hostname = '127.0.0.1'
+sockobj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+port = common_variables.realtime_kalman_port
+
+sockobj.bind(( hostname, port))
+sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+downstream_kalman_hosts = []
+if 1:
+    downstream_kalman_hosts.append( ('astraw-office.kicks-ass.net',28931))
+
+for host in downstream_kalman_hosts:
+    print 'forwarding to',host
+
+class Listener:
+    def run(self):
+        tick = 0
+        gustlength = .05 # seconds
+        print 'listening...'
+
+        while 1:
+            superdatabuf, addr = sockobj.recvfrom(1024)
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+            if 1:
+                # relay the data
+                for host in downstream_kalman_hosts:
+                    sender.sendto(superdatabuf,host)
+
+##             data_packets = flydra_tracker.decode_super_packet(superdatabuf)
+##            print len(data_packets)
+listener = Listener()
+listener.run()
