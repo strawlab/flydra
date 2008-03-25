83d0f0ac367396ead513f6f7ffb22885419437e6
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-03-25 03:56:57 +0000

    separate realtime listener class which seems generally useful
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2275 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/LEDdriver/LED_test_latency.py
+++ flydra/LEDdriver/LED_test_latency.py
@@ -1,44 +1,9 @@
 import flydra.LEDdriver.LEDdriver as LEDdriver
-import time, socket, threading, Queue
+import time, socket
 import flydra.common_variables as common_variables
-import flydra.kalman.flydra_tracker as flydra_tracker
 import numpy
 from optparse import OptionParser
-
-class Listener(threading.Thread):
-    def __init__(self,sockobj):
-        threading.Thread.__init__(self)
-        self.sockobj=sockobj
-        self.quit_now = threading.Event()
-        self.q = Queue.Queue()
-    def run(self):
-        tick = 0
-        print 'listening...'
-        while not self.quit_now.isSet():
-            buf, addr = self.sockobj.recvfrom(1024)
-            data_packets = flydra_tracker.decode_super_packet( buf )
-            for data_packet in data_packets:
-                corrected_framenumber, timestamp, state_vecs,meanP=flydra_tracker.decode_data_packet(data_packet)
-                recv_ts = time.time()
-                self.q.put( (timestamp, state_vecs, recv_ts) )
-                if 0:
-                    #print corrected_framenumber, timestamp, state_vecs
-                    line3d=None
-                    if len(state_vecs):
-                        x,y,z=state_vecs[0][:3]
-                        print x,y,z
-
-    def quit(self):
-        self.quit_now.set()
-
-    def get_listened(self):
-        result = []
-        while 1:
-            try:
-                result.append( self.q.get_nowait() )
-            except Queue.Empty:
-                break
-        return result
+from flydra.realtime_listener import Listener
 
 def doit(no_listen=False,interval=None):
     dev = LEDdriver.Device()
