5785da3410381d7b80bec892212b388278b4b387
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-07-16 20:55:06 +0000

    clip maximum latency when plotting
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2634 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/a2/check_atmel_clock.py
+++ flydra/a2/check_atmel_clock.py
@@ -2,6 +2,7 @@ import tables
 import tables.flavor
 tables.flavor.restrict_flavors(keep=['numpy']) # ensure pytables 2.x
 import numpy
+import numpy as np
 import pylab
 import flydra.analysis.result_utils
 
@@ -16,7 +17,8 @@ if 1:
 ##     filename = 'DATA20070319_204145.h5'
 ##     filename = 'DATA20070319_204348.h5'
 ##     filename = 'DATA20071115_202838.h5'
-    filename = 'DATA20080220_182114.h5'
+#    filename = 'DATA20080220_182114.h5'
+    filename = 'DATA20080715_181455.h5'
     kresults = tables.openFile(filename,mode="r")
     time_model, full_output = flydra.analysis.result_utils.get_time_model_from_data(kresults,
                                                                                     debug=True,
@@ -108,7 +110,12 @@ if 1:
 
     if 1:
         pylab.figure()
-        pylab.hist( latency_msec, bins= 250)
+        if 1:
+            latency_msec_plot = np.clip(latency_msec,0,150.0)
+            print 'clipping maximum latency to 150 msec for plotting purposes'
+        else:
+            latency_msec_plot = latency_msec
+        pylab.hist( latency_msec_plot, bins= 250)
         pylab.title('total latency to 3D reconstruction')
         pylab.ylabel('n occurances')
         pylab.xlabel('latency (msec)')
