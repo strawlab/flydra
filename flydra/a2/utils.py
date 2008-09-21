22b250bd7588207548c89b6a06d3d73dbc79b4d1
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-09-21 20:20:47 +0000

    add get_contig_chunk_idxs()
    Automatically converted from bzr repository with information:
    ------------------------------------------------------------
    revno: 1422
    revision-id: strawman@astraw.com-20080722070653-no4rwg3y2xe6imzt
    parent: strawman@astraw.com-20080721160615-8byaso738i7w57mh
    committer: Andrew Straw <strawman@astraw.com>
    branch nick: flydra-trunk
    timestamp: Tue 2008-07-22 07:06:53 +0000
    message:
      add get_contig_chunk_idxs()
    
    
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2677 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/a2/utils.py
+++ flydra/a2/utils.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy as np
 
 class FastFinder(object):
     """allows fast searching by use of a cached, sorted copy of the original data
@@ -29,3 +30,59 @@ class FastFinder(object):
 
         this_idx = self.idxs[left_idx]
         return this_idx
+
+
+def get_contig_chunk_idxs( arr ):
+    #ADS print 'arr',arr
+    diff = arr[1:]-arr[:-1]
+    #ADS print 'diff',diff
+    non_one = diff != 1
+    #ADS print 'non_one',non_one
+
+    non_one = np.ma.array(non_one).filled()
+    #ADS print 'non_one',non_one
+
+    idxs = np.nonzero(non_one)[0]
+    #ADS print 'idxs',idxs
+    chunk_idxs = []
+    prev_idx = 0
+    for idx in idxs:
+        next_idx = idx+1
+        #ADS print 'idx, prev_idx, next_idx',idx, prev_idx, next_idx
+        data_chunk = np.ma.array(arr[prev_idx:next_idx])
+        #ADS print 'data_chunk',data_chunk
+        if not np.any(data_chunk.mask):
+            chunk_idxs.append( (prev_idx, next_idx) )
+        else:
+            #ADS print 'skipped!'
+            pass
+        prev_idx = next_idx
+    chunk_idxs.append( (prev_idx,len(arr)) )
+    return chunk_idxs
+
+def test_get_contig_chunk_idxs_1():
+    input = np.array( [1,2,3,4,5,  11,12,13,14,15,16, 0, 5, -1], dtype=float)
+    expected = [ (0,5),
+                 (5,11),
+                 (11,12),
+                 (12,13),
+                 (13,14),
+                 ]
+    actual = get_contig_chunk_idxs(input)
+    for i in range(len(expected)):
+        start, stop = expected[i]
+        assert (start,stop)== actual[i]
+
+def test_get_contig_chunk_idxs_2():
+    input = np.array( [np.nan,np.nan,3,4,5,  np.nan,12,13,14,15,16, 0, 5, -1], dtype=float)
+    input = np.ma.masked_where( np.isnan(input), input )
+    expected = [ (2,5),
+                 (6,11),
+                 (11,12),
+                 (12,13),
+                 (13,14),
+                 ]
+    actual = get_contig_chunk_idxs(input)
+    for i in range(len(expected)):
+        start, stop = expected[i]
+        assert (start,stop)== actual[i]
