cad7567ad816a95f34d048465fa74ed0d79da3ed
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-09-25 20:56:50 +0000

    Add test for sympy.
    This was reported as http://code.google.com/p/sympy/issues/detail?id=1123
    
    
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2735 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ src/mahalanobis_sympy_test.py
@@ -0,0 +1,32 @@
+import sympy
+
+S00=sympy.Symbol('S00')
+S01=sympy.Symbol('S01')
+S02=sympy.Symbol('S02')
+S10=sympy.Symbol('S10')
+S11=sympy.Symbol('S11')
+S12=sympy.Symbol('S12')
+S20=sympy.Symbol('S20')
+S21=sympy.Symbol('S21')
+S22=sympy.Symbol('S22')
+
+x0=sympy.Symbol('x0')
+x1=sympy.Symbol('x1')
+x2=sympy.Symbol('x2')
+y0=sympy.Symbol('y0')
+y1=sympy.Symbol('y1')
+y2=sympy.Symbol('y2')
+
+post=sympy.Matrix(([y0-x0],[y1-x1],[y2-x2]))
+S=sympy.Matrix(([S00,S01,S02],[S10,S11,S12],[S20,S21,S22]))
+pre=post.T
+
+maha3=(pre*S*post)[0,0]
+(a,b,c,d,e,f,s)=sympy.symbols('abcdefs')
+tmp = maha3
+tmp = tmp.subs(y0,'a+s*d')
+tmp = tmp.subs(y1,'b+s*e')
+tmp = tmp.subs(y2,'c+s*f')
+print 'solving with sympy',sympy.__version__
+result = sympy.solve(sympy.diff(tmp,s),s)
+print result
