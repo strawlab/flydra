f5f4403c944ac7938af6c06090a3f3f93a05220e
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-11-10 09:23:04 +0000

    move files into attic. make so they do nothing on import
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2962 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/attic/realtime_trigger1.py
+++ flydra/attic/realtime_trigger1.py
@@ -4,27 +4,13 @@ import flydra.kalman.flydra_tracker as flydra_tracker
 import numpy
 import time, math, sys
 
-hostname = '127.0.0.1'
-sockobj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-port = common_variables.realtime_kalman_port
-
-sockobj.bind(( hostname, port))
-sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-downstream_kalman_hosts = []
-if 1:
-    downstream_kalman_hosts.append( ('astraw-office.kicks-ass.net',28931))
-
-
-for host in downstream_kalman_hosts:
-    print 'forwarding to',host
-
 class Listener:
     def run(self):
+        global downstream_kalman_hosts
+
         tick = 0
         print 'listening...'
-        
+
         #sender.sendto('x',('127.0.0.1',common_variables.trigger_network_socket_port))
         post =  0.758, 0.128, 0.244
         post = numpy.array(post)
@@ -36,7 +22,7 @@ class Listener:
             # relay the data
             for host in downstream_kalman_hosts:
                 sender.sendto(databuf,host)
-            
+
             corrected_framenumber, timestamp, state_vecs, meanP=flydra_tracker.decode_data_packet(databuf)
 ##            if len(state_vecs):
 ##                print
@@ -44,7 +30,7 @@ class Listener:
 ##            for i,state_vec in enumerate(state_vecs):
 ##                x,y,z=state_vec[:3]
 ##                print '  obj %d: (%f,%f,%f)'%(i, x,y,z)
-                
+
             now = time.time()
             if abs(now-timestamp) > 0.05:
                 sys.stdout.write('W')
@@ -59,12 +45,31 @@ class Listener:
                     vel = state_vec[3:6]
                     mean_vel = abs( math.sqrt(numpy.sum( vel**2) ) )
                     print 'mean_vel %.2f (m/s), meanP'%(mean_vel,),meanP*1e3
-                    
+
                     #if mean_vel > 0.14:
                     if 1:
                         sender.sendto('x',('127.0.0.1',common_variables.trigger_network_socket_port))
                         last_trig = time.time()
                         print 'trig!', last_trig, state_vec[:3]
-            
-listener = Listener()
-listener.run()
+
+
+if __name__=='__main__':
+    hostname = '127.0.0.1'
+    sockobj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    port = common_variables.realtime_kalman_port
+
+    sockobj.bind(( hostname, port))
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    global downstream_kalman_hosts
+    downstream_kalman_hosts = []
+    if 1:
+        downstream_kalman_hosts.append( ('astraw-office.kicks-ass.net',28931))
+
+
+    for host in downstream_kalman_hosts:
+        print 'forwarding to',host
+
+    listener = Listener()
+    listener.run()
--- flydra/attic/realtime_trigger_xmit.py
+++ flydra/attic/realtime_trigger_xmit.py
@@ -4,23 +4,10 @@ import flydra.kalman.flydra_tracker as flydra_tracker
 import numpy
 import time, math, sys
 
-hostname = '127.0.0.1'
-sockobj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-port = common_variables.realtime_kalman_port
-
-sockobj.bind(( hostname, port))
-sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-downstream_kalman_hosts = []
-if 1:
-    downstream_kalman_hosts.append( ('astraw-office.kicks-ass.net',28931))
-
-for host in downstream_kalman_hosts:
-    print 'forwarding to',host
 
 class Listener:
     def run(self):
+        global downstream_kalman_hosts
         tick = 0
         gustlength = .05 # seconds
         print 'listening...'
@@ -37,5 +24,23 @@ class Listener:
 
 ##             data_packets = flydra_tracker.decode_super_packet(superdatabuf)
 ##            print len(data_packets)
-listener = Listener()
-listener.run()
+
+if __name__=='__main__':
+    global downstream_kalman_hosts
+    hostname = '127.0.0.1'
+    sockobj = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    port = common_variables.realtime_kalman_port
+
+    sockobj.bind(( hostname, port))
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    downstream_kalman_hosts = []
+    if 1:
+        downstream_kalman_hosts.append( ('astraw-office.kicks-ass.net',28931))
+
+    for host in downstream_kalman_hosts:
+        print 'forwarding to',host
+
+    listener = Listener()
+    listener.run()
