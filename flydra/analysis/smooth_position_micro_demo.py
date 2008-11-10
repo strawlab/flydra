4ee02168da0fe99973b3b1016f52e077cb8dba3e
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-11-10 09:23:05 +0000

    do nothing on import
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@2963 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/analysis/smooth_position_micro_demo.py
+++ flydra/analysis/smooth_position_micro_demo.py
@@ -4,19 +4,20 @@ from pylab import linspace
 import PQmath
 from numpy.random import normal
 
-if 0:
-    P = nx.array( [[1, 2, 3, 4, 5, 6],
-                   [1, 2, 3, 4, 5, 6],
-                   [1, 2.1, 3.01, 4, 5, 6]] )
-    P.transpose()
-else:
-    t = linspace(0, 10*math.pi, 400 )
-    P = nx.sin(t)[:,nx.newaxis]
-    P = P + normal( 0, .1, P.shape )
+if __name__=='__main__':
+    if 0:
+        P = nx.array( [[1, 2, 3, 4, 5, 6],
+                       [1, 2, 3, 4, 5, 6],
+                       [1, 2.1, 3.01, 4, 5, 6]] )
+        P.transpose()
+    else:
+        t = linspace(0, 10*math.pi, 400 )
+        P = nx.sin(t)[:,nx.newaxis]
+        P = P + normal( 0, .1, P.shape )
 
-Pstar = PQmath.smooth_position( P, 0.01, 0.5, 1e-9, 1e12)
+    Pstar = PQmath.smooth_position( P, 0.01, 0.5, 1e-9, 1e12)
 
-from pylab import *
-plot( t, P, 'b-' )
-plot( t, Pstar, 'r-' )
-show()
+    from pylab import *
+    plot( t, P, 'b-' )
+    plot( t, Pstar, 'r-' )
+    show()
