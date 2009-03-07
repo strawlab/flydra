5cba29fc9196f883fde85d59588066a809b0c135
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2009-03-07 02:01:00 +0000

    flydra: improvements to FastFinder
    git-svn-id: https://code.astraw.com/kookaburra/trunk/flydra@3362 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

--- flydra/a2/fastfinder_help.pyx
+++ flydra/a2/fastfinder_help.pyx
@@ -2,9 +2,10 @@ import numpy as np
 cimport numpy as np
 
 import cython
+import utils # flydra.a2.utils
 
 @cython.boundscheck(False) # turn of bounds-checking for entire function
-def get_first_idx(np.ndarray[long, ndim=1] haystack, np.ndarray[long, ndim=1] needles):
+def get_first_idx_long(np.ndarray[long, ndim=1] haystack, np.ndarray[long, ndim=1] needles, int missing_ok=0):
 
     # TODO: implementation that does binary search on pre-sorted
     # haystack
@@ -31,7 +32,42 @@ def get_first_idx(np.ndarray[long, ndim=1] haystack, np.ndarray[long, ndim=1] ne
         else:
             found[i] = -1
 
-    if np.any(found==-1):
-        raise ValueError('some of your needles were not found')
+    if not missing_ok:
+        if np.any(found==-1):
+            raise utils.MissingValueError('some of your needles were not found')
+
+    return found
+
+@cython.boundscheck(False) # turn of bounds-checking for entire function
+def get_first_idx_double(np.ndarray[double, ndim=1] haystack, np.ndarray[double, ndim=1] needles, int missing_ok=0):
+
+    # TODO: implementation that does binary search on pre-sorted
+    # haystack
+
+    assert haystack.ndim==1
+    assert needles.ndim==1
+
+    cdef long hmax = haystack.shape[0]
+    cdef long nmax = needles.shape[0]
+    cdef long i,j
+    cdef int needle_found
+    cdef np.ndarray[long, ndim=1] found
+
+    found = np.zeros( (needles.shape[0],) , dtype=np.int)
+
+    for i in range(nmax):
+        needle_found = 0
+        for j in range(hmax):
+            if haystack[j]==needles[i]:
+                needle_found=1
+                break
+        if needle_found:
+            found[i] = j
+        else:
+            found[i] = -1
+
+    if not missing_ok:
+        if np.any(found==-1):
+            raise utils.MissingValueError('some of your needles were not found')
 
     return found
