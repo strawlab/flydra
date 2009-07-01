e07124626178940ae77f0d5d5412dc06b544677f
Andrew Straw <strawman@astraw.com>
2009-07-01 12:21:27 -0700

    test cleanup: rename file to remove 'test' directory

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(name='flydra',
 
 # testing
     'flydra_test_commands = flydra.test_commands:main',
-    'flydra_test_mpl_markersize = flydra.test.mpl_markersize:main',
+    'flydra_test_mpl_markersize = flydra.mpl_markersize:main',
     ],
 
     'gui_scripts': [
