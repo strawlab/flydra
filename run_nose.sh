f160f7f14813edbfd020d9bafd20361ba20f6f1a
Andrew Straw <strawman@astraw.com>
2009-07-01 10:40:55 -0700

    test cleanup: attempt to make Xfvb running more robust

--- run_nose.sh
+++ run_nose.sh
@@ -19,11 +19,23 @@ easy_install nose-0.11.1.tar.gz
 # Compile and then install into virtual environment
 python setup.py develop
 
+
+#----- Now, run tests -----
+
+# check for expected permissions on X server
+python -c 'import os; assert os.path.exists("/etc/X2.hosts")'
+python -c 'fd = open("/etc/X2.hosts",mode="r"); assert "localhost\n" in fd.readlines()'
+
 # run X server
 Xvfb :2 &
 # get PID
 XVFBPID=$!
 echo "Xvfb running in process $XVFBPID"
+
+echo "checking that Xvfb process is running"
+sleep 1
+python -c "import os; assert os.path.exists(\"/proc/$XVFBPID\")"
+echo "PID $XVFBPID seems OK, will continue with tests"
 
 # Run tests, capture exit code, don't quit on error.
 set +o errexit
