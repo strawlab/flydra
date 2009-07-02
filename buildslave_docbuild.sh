dc8a71e7f732cd1af339be9cd67c199658377409
Andrew Straw <strawman@astraw.com>
2009-07-01 20:10:38 -0700

    allow build slaves to build docs automatically

---
+++ buildslave_docbuild.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# abort on error
+set -o errexit
+
+VIRTUALENVDIR=PYtest
+
+# activate new virutal environment
+source $VIRTUALENVDIR/bin/activate
+
+# need to install our own sphinx into virtualenv so it picks up flydra
+easy_install Jinja2-2.1.1.tar.gz
+easy_install Sphinx-0.6.2.tar.gz
+
+cd flydra-sphinx-docs/
+./get-svn.sh
+
+echo "PATH=$PATH"
+echo "PYTHONPATH=$PYTHONPATH"
+
+# clean
+rm -rf build
+
+# make
+make latex
+cd build/latex
+make all-pdf
+cd ../..
+make html
