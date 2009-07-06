7c60e1dbd4f13084c7d5d575d65f76048e8e7fcf
Andrew Straw <strawman@astraw.com>
2009-07-06 15:18:23 -0700

    save as .ufmf v3

--- flydra/camnode.py
+++ flydra/camnode.py
@@ -1206,7 +1206,7 @@ class SaveSmallData(object):
                             raise NotImplementedError(
                                 'unable to determine shape from image with '
                                 'coding %s'%(chainbuf.image_coding,))
-                        self._ufmf = ufmf.UfmfSaverV2( filename,
+                        self._ufmf = ufmf.UfmfSaverV3( filename,
                                                        coding = chainbuf.image_coding,
                                                        max_width=w,
                                                        max_height=h,
