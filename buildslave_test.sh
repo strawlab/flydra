e5512beba736ecc08bea93bb8d27abe5c97e1475
Andrew Straw <strawman@astraw.com>
2009-12-20 06:35:33 -0800

    buildbot: read xvfb display from file to allow multiple simultaneous test instances

--- buildslave_test.sh
+++ buildslave_test.sh
@@ -14,11 +14,13 @@ easy_install nose-0.11.1.tar.gz
 #----- Now, run tests -----
 
 # check for expected permissions on X server
-python -c 'import os; assert os.path.exists("/etc/X2.hosts"), "no /etc/X2.hosts file exists for xvfb"'
-python -c 'fd = open("/etc/X2.hosts",mode="r"); assert "localhost\n" in fd.readlines(), "no localhost in /etc/X2.hosts"'
+python -c 'import os; assert os.path.exists(os.path.expanduser('~/.xvfb-display')), "no ~/.xvfb-display"'
+python -c 'import os; d=open(os.path.expanduser('~/.xvfb-display')).read()[1:]; assert os.path.exists("/etc/X%d.hosts"%d), "no /etc/X%d.hosts file exists for xvfb %d"'
+python -c 'import os; d=open(os.path.expanduser('~/.xvfb-display')).read()[1:]; fd = open("/etc/X%d.hosts"%d,mode="r"); assert "localhost\n" in fd.readlines(), "no localhost in /etc/X%d.hosts"%d'
 
 # run X server
-Xvfb :2 &
+export DISPLAY=`cat ~/.xvfb-display`
+Xvfb $DISPLAY &
 # get PID
 XVFBPID=$!
 echo "Xvfb running in process $XVFBPID"
@@ -30,9 +32,9 @@ echo "PID $XVFBPID seems OK, will continue with tests"
 
 # Run tests, capture exit code, don't quit on error.
 set +o errexit
-#DISPLAY=":2" nosetests --verbosity=3 -A "not slow_command"
-DISPLAY=":2" nosetests -A "not known_fail" --all-modules
-#DISPLAY=":2" nosetests --verbosity=3
+#nosetests --verbosity=3 -A "not slow_command"
+nosetests -A "not known_fail" --all-modules
+#nosetests --verbosity=3
 RESULT=$?
 set -o errexit
 
