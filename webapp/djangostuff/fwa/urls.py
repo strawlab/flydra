15826685167e1b4fb5ce62e437736d572765ecf4
Andrew Straw <strawman@astraw.com>
2010-02-20 11:26:14 +0000

    updates

--- webapp/djangostuff/fwa/urls.py
+++ webapp/djangostuff/fwa/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('fwa.views',
-    (r'db/', 'db_index'),
+    (r'db/$', 'db_index'), # show list of databases, choose the database
+    (r'db/(?P<db_name>\w+)/$', 'db'), # show the datasets in database, choose dataset
+    (r'db/(?P<db_name>\w+)/(?P<dataset>\w+)/$', 'dataset'), # show the dataset
 )
--- webapp/djangostuff/fwa/views.py
+++ webapp/djangostuff/fwa/views.py
@@ -1,7 +1,8 @@
-from django.template import Context, loader
+from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.views.generic.simple import redirect_to
 
 from couchdb.client import Server
 import couchdb.http
@@ -19,7 +20,7 @@ def is_access_valid(db_name,user):
 
     db_id = 'database:%s'%db_name
     db_doc = metadb[db_id]
-    
+
     valid_groups = list(set(user_doc['groups']).intersection( set( db_doc['groups'])))
     if len(valid_groups):
         return True
@@ -34,10 +35,52 @@ def db_index(request):
         valid_db_names = [db_name for db_name in db_names if is_access_valid(db_name,request.user)]
     except couchdb.http.ResourceNotFound,err:
         t = loader.get_template('error.html')
-        c = Context({'description':'The django user %s was not found in the '
-                     'flydraweb_metadata database.'%request.user})
+        c = RequestContext(request,
+                           {'description':'The django user %s was not found in the '
+                            'flydraweb_metadata database.'%request.user})
         return HttpResponse(t.render(c))
 
-    t = loader.get_template('db.html')
-    c = Context({"user":request.user,"valid_db_names":valid_db_names})
+    if len(valid_db_names)==1:
+        # no need to choose manually, automatically fast-forward
+        return redirect_to(request,valid_db_names[0],permanent=False)
+
+    t = loader.get_template('db_index.html')
+    c = RequestContext(request, {"valid_db_names":valid_db_names})
     return HttpResponse(t.render(c))
+
+@login_required
+def db(request,db_name=None):
+    """view a db, which means select a dataset"""
+    assert db_name is not None
+    assert is_access_valid(db_name,request.user)
+
+    couch_server = Server(couchbase)
+    db = couch_server[db_name]
+
+    view_results = db.view('fw/datasets')
+    #raise str( type(view_result) ) + ' ' + str(view_result)
+
+    if len(view_results)==1:
+        # only one dataset, choose it
+        row = view_results.rows[0]
+
+        dataset_id = row.id
+        #raise dataset_id
+        return redirect_to(request,dataset_id,permanent=False)
+        #return redirect_to(request,'noexist',permanent=False)
+    1/0
+
+    t = loader.get_template('db.html')
+    c = RequestContext(request)
+    return HttpResponse(t.render(c))
+
+@login_required
+def dataset(request,db_name=None,dataset=None):
+    assert db_name is not None
+    assert dataset is not None
+    assert is_access_valid(db_name,request.user)
+
+    t = loader.get_template('dataset.html')
+    c = RequestContext(request)
+    return HttpResponse(t.render(c))
+
