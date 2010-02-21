b008b2c3ec24bf95d9c7552c3a71cd98f35621b1
Andrew Straw <strawman@astraw.com>
2010-02-21 22:25:43 +0000

    copy evently profile widget from taskr

---
+++ webapp/couchstuff/couchapp_metadb/evently/profile/profileReady/selectors/form/submit.js
@@ -0,0 +1,19 @@
+function() {
+  var profile = $$("#profile").profile;
+  // $.log("profile", profile, this);
+  var texta = $("textarea[name=body]", this);
+  var newTask = {
+    body : texta.val(),
+    type : "task",
+    created_at : new Date(),
+    authorProfile : profile
+  };
+  // works because app is attached all the way down
+  // maybe we need a better way to support shared state?
+  $$(this).app.db.saveDoc(newTask, {
+    success : function() {
+      texta.val('');
+    }
+  });
+  return false;
+}
