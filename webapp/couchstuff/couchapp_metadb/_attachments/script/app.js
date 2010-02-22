ffc2452660154ecb4eeaaa18764fb94e58ba2ad5
Andrew Straw <strawman@astraw.com>
2010-02-22 00:18:33 -0800

    disable a bunch of logging

--- webapp/couchstuff/couchapp_metadb/_attachments/script/app.js
+++ webapp/couchstuff/couchapp_metadb/_attachments/script/app.js
@@ -1,9 +1,7 @@
 // $.couch.app() loads the design document from the server and 
 // then calls our application.
 $.couch.app(function(app) {
-    console.log("ads 1");
-    console.log(app);
-    console.log("ads 2");
+    //console.log(app);
 
   // customize the couchapp profile widget with our templates and selectors
   $.extend(true, 
@@ -11,9 +9,6 @@ $.couch.app(function(app) {
     app.ddoc.evently.profile);
   
   // apply the widget to the dom
-  $.log("this is app.ddoc.vendor.couchapp.evently.profile: -----------");
-  $.log(app.ddoc.vendor.couchapp.evently.profile);
-  $.log("-------that was app.ddoc.vendor.couchapp.evently.profile");
   $("#profile").evently(app.ddoc.vendor.couchapp.evently.profile, app);
   
   // setup the account widget
@@ -23,16 +18,9 @@ $.couch.app(function(app) {
   $.evently.connect($("#account"), $("#profile"), ["loggedIn", "loggedOut"]);
   
   // now set up the main list of flydra_dbs
-    $.log("ads 3");
   var flydra_dbs = app.ddoc.evently.flydra_dbs;
-    $.log("ads 4");
-  
-  $.log(flydra_dbs)
-    $.log("ads 4.5");
   $("#flydra_dbs").evently(flydra_dbs, app);
-    $.log("ads 5");
   $.pathbinder.begin("/");
-    $.log("ads 6");
 });
 
 // todo move to a plugin somewhere
