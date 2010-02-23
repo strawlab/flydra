a7ab49b7c5b9dabc8f65d78099674bcf18616d04
Andrew Straw <strawman@astraw.com>
2010-02-23 05:08:43 -0800

    attempt to sort ufmfs better

--- webapp/couchstuff/analysis_couchapp/views/ufmfs/map.js
+++ webapp/couchstuff/analysis_couchapp/views/ufmfs/map.js
@@ -1,10 +1,7 @@
 function(doc) {
-    if (doc.type=="dataset") {
-        emit([doc._id,0], null);
-    }
     if (doc.type=="ufmf") {
         if (doc.dataset != null) {
-            emit([doc.dataset,1], null);
+            emit([doc.start_time,doc.stop_time], null);
         }
     }
 };
