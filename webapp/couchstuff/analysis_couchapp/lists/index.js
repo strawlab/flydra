41c5f5f77a7192b9d5654e92abc2ce3f281c5be1
Andrew Straw <strawman@astraw.com>
2010-02-22 20:15:56 -0800

    convert templating to server-side mustache

--- webapp/couchstuff/analysis_couchapp/lists/index.js
+++ webapp/couchstuff/analysis_couchapp/lists/index.js
@@ -1,50 +1,49 @@
 function(head, req) {
     // !json templates.index
-    // !code lib/JSON.js
     // !code vendor/couchapp/path.js
-    // !code vendor/couchapp/template.js
+    // !code lib/mustache.js
+    // !code lib/human_sizes.js
 
     var indexPath = listPath('index','datasets');
 
     provides("html", function() {
         start({"headers":{"Content-Type" : "text/html; charset=utf-8"}});
 
-        // render the html head using a template
-        send(template(templates.index.head, {
-            title : "Flydra analysis",
-            index : indexPath,
-            assets : assetPath()
-        }));
-        
+        var row_view = {
+            datasets : []
+        }
+
+        function size_format (filesize) {
+            return getSize(filesize,1,0);
+        }
+
+        var make_dataset = function(r, path) {
+            return {
+                path : path,
+                dataset_id : r.value.dataset_doc._id,
+                dataset_name : r.value.dataset_doc.name,
+                ufmf_bytes_human : size_format(r.value.ufmf_bytes),
+                h5_bytes_human : size_format(r.value.h5_bytes),
+                ufmf_files : r.value.ufmf_files,
+                h5_files : r.value.h5_files
+            };
+        }
+
         while (row = getRow()) {
-            // XXX This only works for "datasets" view.
-            //send( '<li>'+JSON.stringify(row) + '</li>\n' );
-            var doc = row.value.dataset_doc;
-            //var row_url = showPath('show_dataset', doc._id);
-
-            send(template(templates.index.row, {
-                link : showPath('show_dataset', doc._id),
-                title : doc.name,
-                dataset_size : "",
-                summary : "",
-                assets : assetPath()
-            }));
-            /*
-            //var myshow_url = doc._id;
-            var myshow_url = showPath('show_dataset', doc._id);
-        
-            send( '  <li> <a href="' + myshow_url + '">' + doc.name + '</a>\n' +
-                  '       &#123; h5: {{h5_files}} files ({{h5_bytes_human}}),\n' +
-                  '       ufmf: {{ufmf_files}} files ({{ufmf_bytes_human}}) &#125;\n' +
-                  '  </li>\n');
-            */
+            //var doc = row.value.dataset_doc;
+            var myshow_url = showPath('show_dataset', row.value.dataset_doc._id);
+            row_view.datasets.push( make_dataset( row, myshow_url ));
         };
-        //send( '</ul>\n' );
 
-        // render the html tail template
-        return template(templates.index.tail, {
-            assets : assetPath()
-        });
+        var content = Mustache.to_html(templates.index.rows, row_view);
+
+        var view = {
+            title: "Flydra analysis",
+            index: indexPath,
+            content : content
+        }
+
+        return Mustache.to_html(templates.index.must, view);
 
     });
 }
