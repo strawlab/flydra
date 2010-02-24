a037d8c488ea473e289d954039aa2dd098b46808
Andrew Straw <strawman@astraw.com>
2010-02-23 23:24:23 -0800

    DataNode view adapts h5 files

--- webapp/couchstuff/analysis_couchapp/views/DataNode/map.js
+++ webapp/couchstuff/analysis_couchapp/views/DataNode/map.js
@@ -5,6 +5,7 @@ function(doc) {
 
     switch (doc.type) {
     case "h5":
+        // adapt to DataNode
         is_data_node = true;
         if (doc.has_2d_position) {
             props = props.concat( "2d position" );
@@ -21,28 +22,23 @@ function(doc) {
         if (doc.has_calibration) {
             props = props.concat( "calibration" );
         }
-        break;
-    case "ufmf":
-        is_data_node = true;
-        props = props.concat( "ufmf" );
-        break;
-    default:
-        break;
-    }
 
-    if (is_data_node) {
-        if (typeof(doc.start_time)=="undefined") {
-            is_data_node = false;
-        }
-    }
-
-    if (is_data_node) {
         var value = { properties : props,
                       start_time : doc.start_time,
                       stop_time  : doc.stop_time,
                       dataset    : doc.dataset,
                       sources    : [doc._id],
                     }
-        emit( null, value );
+        break;
+    case "datanode":
+        // already implements DataNode
+        value = doc;
+        break;
+    default:
+        break;
+    }
+
+    if (is_data_node) {
+        emit( [value.dataset, value.properties], value );
     }
 }
