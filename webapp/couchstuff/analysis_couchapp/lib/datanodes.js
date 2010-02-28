7f4dc78493963e889c27f6c48dc8544e5ba99798
Andrew Straw <strawman@astraw.com>
2010-02-28 03:12:42 +0000

    refactor datanode code into library function

---
+++ webapp/couchstuff/analysis_couchapp/lib/datanodes.js
@@ -0,0 +1,55 @@
+var parse_doc = function(doc) {
+    
+    var success = true;
+    var props = [];
+    var value;
+
+    switch (doc.type) {
+    case "h5":
+        // adapt to DataNode
+        if (doc.has_2d_position) {
+            props = props.concat( "2d position" );
+        }
+        if (doc.has_2d_orientation) {
+            props = props.concat( "2d orientation" );
+        }
+        if (doc.has_3d_position) {
+            props = props.concat( "3d position" );
+        }
+        if (doc.has_3d_orientation) {
+            props = props.concat( "3d orientation" );
+        }
+        if (doc.has_calibration) {
+            props = props.concat( "calibration" );
+        }
+
+        value = { properties : props,
+                  start_time : doc.start_time,
+                  stop_time  : doc.stop_time,
+                  dataset    : doc.dataset,
+                  sources    : [doc._id],
+		  status_tags: ["built"],
+                }
+        break;
+    case "calibration":
+        value = { properties : ["calibration"],
+                  dataset    : doc.dataset,
+                  sources    : [doc._id],
+		  status_tags: ["built"],
+                }
+        break;
+    case "datanode":
+        // already implements DataNode
+        value = doc;
+        break;
+    default:
+        success = false;
+        break;
+    }
+
+    if (success) {
+	return value;
+    } else {
+	return null;
+    }
+}
--- webapp/couchstuff/analysis_couchapp/views/DataNode/map.js
+++ webapp/couchstuff/analysis_couchapp/views/DataNode/map.js
@@ -1,53 +1,8 @@
 function(doc) {
+    // !code lib/datanodes.js
 
-    var success = true;
-    var props = [];
-    var value;
-
-    switch (doc.type) {
-    case "h5":
-        // adapt to DataNode
-        if (doc.has_2d_position) {
-            props = props.concat( "2d position" );
-        }
-        if (doc.has_2d_orientation) {
-            props = props.concat( "2d orientation" );
-        }
-        if (doc.has_3d_position) {
-            props = props.concat( "3d position" );
-        }
-        if (doc.has_3d_orientation) {
-            props = props.concat( "3d orientation" );
-        }
-        if (doc.has_calibration) {
-            props = props.concat( "calibration" );
-        }
-
-        value = { properties : props,
-                  start_time : doc.start_time,
-                  stop_time  : doc.stop_time,
-                  dataset    : doc.dataset,
-                  sources    : [doc._id],
-		  status_tags: ["built"],
-                }
-        break;
-    case "calibration":
-        value = { properties : ["calibration"],
-                  dataset    : doc.dataset,
-                  sources    : [doc._id],
-		  status_tags: ["built"],
-                }
-        break;
-    case "datanode":
-        // already implements DataNode
-        value = doc;
-        break;
-    default:
-        success = false;
-        break;
-    }
-
-    if (success) {
+    var value = parse_doc(doc);
+    if (value!=null) {
         emit( [value.dataset, value.properties, value.status_tags], value );
     }
 }
