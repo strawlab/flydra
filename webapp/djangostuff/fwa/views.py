ea894d212b2936f3989cd870ddbb2fafc36092c9
Andrew Straw <strawman@astraw.com>
2010-04-18 18:46:34 +0000

    remove Django proxy for CouchDB, which got hop-by-hop errors

--- webapp/djangostuff/fwa/views.py
+++ webapp/djangostuff/fwa/views.py
@@ -210,38 +210,6 @@ def datanodes_by_property(request,db_name=None,dataset=None,property_name=None,c
     c = RequestContext(request,context)
     return HttpResponse(t.render(c))
 
-
-class JsonHttpResponse(HttpResponse):
-    def __init__(self, data):
-        content = simplejson.dumps(data,
-                                   indent=2,
-                                   ensure_ascii=False)
-        super(JsonHttpResponse, self).__init__(content=content,
-                                           mimetype='application/json; charset=utf8')
-
-
-@login_required
-def couch_proxy(orig_request,couch_path=None):
-    if couch_path is None:
-        couch_path = ''
-    uri = settings.FWA_COUCH_BASE_URI + '/' + couch_path
-    new_request = urllib2.Request(uri)
-    if 'HTTP_ACCEPT' in orig_request.META:
-        new_request.add_header('Accept', orig_request.META['HTTP_ACCEPT'])
-    try:
-        f = urllib2.urlopen(new_request)
-    except urllib2.HTTPError,err:
-        return HttpResponse(content='error %s'%(err.code,),status=err.code)
-    meta = f.info()
-
-    result = HttpResponse(content=f.read(),
-                          # should also be set below, so this may be redundant
-                          content_type=meta['Content-Type'],
-                          )
-    for header in meta.keys():
-        result[header] = meta[header]
-    return result
-
 @login_required
 def apply_analysis_type(request,db_name=None,dataset=None,class_name=None):
     dataset_id = 'dataset:'+dataset
