fbdca1442d31e3a7d0a0428b8f9167cfe6f71b4a
Andrew Straw <strawman@astraw.com>
2010-04-18 20:49:17 +0000

    Revert "remove Django proxy for CouchDB, which got hop-by-hop errors"
    This reverts commit ea894d212b2936f3989cd870ddbb2fafc36092c9.

--- webapp/djangostuff/fwa/urls.py
+++ webapp/djangostuff/fwa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('fwa.views',
     (r'^$', 'select_db'), # show list of databases, choose the database
+    (r'^couch-proxy/(?P<couch_path>.*)/$', 'couch_proxy'), # proxy for couchdb
     (r'^(?P<db_name>\w+)/$', 'select_dataset'), # show the datasets in database, choose dataset
     (r'^(?P<db_name>\w+)/(?P<dataset>\w+)/$', 'dataset'), # show the dataset
     (r'^(?P<db_name>\w+)/(?P<dataset>\w+)/DataNodes/$', 'datanodes_by_property'), # show the dataset
