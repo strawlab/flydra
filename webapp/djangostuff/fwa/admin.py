58df6843bf1d8447d2f2e5021338643c7b709371
Andrew Straw <strawman@astraw.com>
2010-04-18 17:27:35 +0000

    use Django ORM to store CouchDB passwords

---
+++ webapp/djangostuff/fwa/admin.py
@@ -0,0 +1,4 @@
+from models import CouchDB_Database
+from django.contrib import admin
+
+admin.site.register(CouchDB_Database)
--- webapp/djangostuff/fwa/models.py
+++ webapp/djangostuff/fwa/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class CouchDB_Database(models.Model):
+    name     = models.CharField(max_length=200)
+    user     = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
