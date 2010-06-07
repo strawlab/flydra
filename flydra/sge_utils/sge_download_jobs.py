8207316017456738286543efa95fb89f954873f8
Andrew Straw <strawman@astraw.com>
2010-06-07 15:02:08 +0000

    webapp: submit job with SGE job name and SGE job dependencies

--- flydra/sge_utils/sge_download_jobs.py
+++ flydra/sge_utils/sge_download_jobs.py
@@ -25,8 +25,15 @@ def download_jobs(couch_url, db_name):
 
         doc = db[job_id]
         doc['state'] = states.SUBMITTED
+        
+        job_depends = ','.join( doc.get('job_depends', [] ) )
+        if len(job_depends):
+            job_depends = '-hold_jid '+job_depends
+
+        job_name = '-N job%s'%job_id
         # XXX use virtualenv in ~/PY
-        cmd = 'qsub -b y ~/PY/bin/flydra_sge_run_job %s %s %s'%(couch_url, db_name, job_id)
+        cmd = 'qsub -b y %s %s ~/PY/bin/flydra_sge_run_job %s %s %s'%(job_name, job_depends,
+                                                                      couch_url, db_name, job_id)
         subprocess.check_call(cmd,shell=True)
         db.update([doc])
 
