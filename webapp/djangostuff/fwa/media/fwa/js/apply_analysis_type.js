7f7145c5960fe3cd9612bad822cb51f7b9ee631a
Andrew Straw <strawman@astraw.com>
2010-08-28 02:03:21 +0000

    javascript function to computer based on realtime selection working

--- webapp/djangostuff/fwa/media/fwa/js/apply_analysis_type.js
+++ webapp/djangostuff/fwa/media/fwa/js/apply_analysis_type.js
@@ -1,13 +1,26 @@
 function sources_selection_changed(event, ui) { 
-    console.log("selected 3");
+    var result = $("#select-result").empty();
+    var myid = this.id;
+    $(".ui-selected", this).each(function(){
+	    var index = $("#" + myid + " li").index(this);
+	    result.append(" #" + (index + 1));
+	});
+
  }
+
+
+function no_space(str) {
+    return str.replace(" ","_");
+}
 
 function FWA_create_ul_for_items( elem ) {
 	var primary;
 
 	if (typeof js_client_info_global.dominant_source_node_type === 'undefined') {
-	    // TODO: choose the one with most options
-	    primary = js_client_info_global.sources[0];
+	    for (primary in js_client_info_global.sources) {
+		// set the variable "primary" to the first key
+		break;
+	    }
 	} else {
 	    primary = js_client_info_global.dominant_source_node_type;
 	}
@@ -15,17 +28,26 @@ function FWA_create_ul_for_items( elem ) {
 
 	var myitems = "";
 	for (var rownum in js_client_info_global.sources[primary].rows) {
-	    var myid = js_client_info_global.sources[primary].rows[rownum]._id
+	    var row = js_client_info_global.sources[primary].rows[rownum];
+	    var myid = row.id;
 	    myitems = myitems + "<li class=\"ui-widget-content\">" + myid + "</li>";
 	}
-	elem.append("<h2>Select data sources</h2>");
+
+	var my_id = "selectable_" + no_space(primary);
+	elem.append("<h2>Select data "+primary+" sources</h2>");
+	elem.append("You've selected: <span id=\"select-result\">none</span>.");
 	elem.append("<p>Hold ctrl (on Mac: command) and click to select multiple sources.</p>");
-	elem.append( "<ul id=\"selectable\"> " + myitems + "</ul>" );
-	$("#selectable").selectable({selected: sources_selection_changed,
-		    unselected: sources_selection_changed});
+	elem.append( "<div class=\"fwa_selectable\">")
+	elem.append( "<ul id=\"" + my_id + "\" class=\"ui-selectable\"> " + myitems + "</ul>" );
+	elem.append( "</div>" )
+	$("#"+my_id).selectable({stop: sources_selection_changed});
 }
 
 $(document).ready(function(){
+
+	if (typeof js_client_info_global === 'undefined') {
+	    return;
+	}
 
 	// With this javascript running, disable user input into all but
 	// dominant selection box. This code will fill the rest.
@@ -35,6 +57,7 @@ $(document).ready(function(){
 	    var eid = js_client_info_global.sources[source_node_type].select_id;
 	    var selector = "#" + eid;
 	    var dom = $(selector);
+
 	    // dom is just the <select> box, hide its parent (the <p>)
 	    dom.parent().hide();
 	}
