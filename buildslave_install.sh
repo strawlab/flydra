21c736307f23a02fa619d766183cd4d1862605df
Andrew Straw <strawman@astraw.com>
2011-02-17 15:05:52 +0100

    buildbot: don't use easy_install

--- buildslave_install.sh
+++ buildslave_install.sh
@@ -14,9 +14,6 @@ virtualenv $VIRTUALENVDIR
 # activate new virutal environment
 source $VIRTUALENVDIR/bin/activate
 
-easy_install numpy>=1.3
-easy_install numexpr>=1.3
-
 # Compile and then install into virtual environment
 cython flydra/camnode_colors.pyx
 python setup.py develop
