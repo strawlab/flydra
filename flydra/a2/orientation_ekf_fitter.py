724743641eea22484f5afbafc2a0b5f8080208f0
Andrew Straw <strawman@astraw.com>
2011-04-28 22:51:51 +0200

    analysis: orientation_ekf_fitter: improve error message

--- flydra/a2/orientation_ekf_fitter.py
+++ flydra/a2/orientation_ekf_fitter.py
@@ -968,8 +968,8 @@ def compute_ori_quality(kh5, orig_frames, obj_id, smooth_len=10):
     try:
         table = getattr(group,'obj%d'%obj_id)
     except:
-        sys.stderr.write('ERROR while opening %s\n'%kh5.filename)
-        sys.stderr.write('Hint: re-run orientation fitting for this file.\n')
+        sys.stderr.write('ERROR while getting EKF fit data for obj_id %d in file opening %s\n'%(obj_id,kh5.filename))
+        sys.stderr.write('Hint: re-run orientation fitting for this file (for this obj_id).\n')
         raise
     table_ram = table[:]
     frames = table_ram['frame']
