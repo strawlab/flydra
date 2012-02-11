3cbf377998c81559eaeaaddb0610d09554f2216a
Andrew Straw <strawman@astraw.com>
2012-02-11 23:39:33 +0100

    update Sphinx version

--- buildslave_docbuild.sh
+++ buildslave_docbuild.sh
@@ -12,7 +12,7 @@ source $VIRTUALENVDIR/bin/activate
 
 # need to install our own sphinx into virtualenv so it picks up flydra
 easy_install Jinja2-2.1.1.tar.gz
-easy_install Sphinx-1.0.7.tar.gz
+easy_install Sphinx-1.1.2.tar.gz
 
 cd flydra-sphinx-docs/
 
