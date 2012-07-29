3b9de9df92c35227badb6fe0f72560836a5f2e0d
Andrew Straw <strawman@astraw.com>
2012-07-29 23:11:15 +0200

    remove firmware replaced by fview_ext_trig/CamTrigUSB

--- at90usb_trigger/firmware/README.txt
+++
@@ -1,2 +0,0 @@
-Timer3: PWM/OCR A: primary camera ongoing trigger output
-        PWM/OCR B,C: reserved future ongoing trigger outputs
--- at90usb_trigger/firmware/adc_sampling.h
+++
@@ -1,8 +0,0 @@
-#ifndef _adc_samping_h_
-#define _adc_samping_h_
-
-void init_ADC_sampling(void);
-
-void start_ADC_sample(void);
-
-#endif /* _adc_samping_h_ */
