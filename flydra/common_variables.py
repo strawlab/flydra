0de2c69cbef4b2baa26401635d37085f5bfe0ecc
John Stowers <john.stowers@gmail.com>
2013-04-23 18:29:52 +0200

    flydra: put the timestamp echo on a lower port to fix an odd race with ROS

--- flydra/common_variables.py
+++ flydra/common_variables.py
@@ -1,9 +1,9 @@
 MINIMUM_ECCENTRICITY = 1.4 # threshold to fit line
 
-timestamp_echo_listener_port = 28992 # on cameras
+timestamp_echo_listener_port = 9992 # on cameras
 timestamp_echo_fmt1 = '<d'
 timestamp_echo_fmt_diff = '<d'
-timestamp_echo_gatherer_port = 28993 # on MainBrain
+timestamp_echo_gatherer_port = 9993 # on MainBrain
 timestamp_echo_fmt2 = '<dd'
 
 recv_pt_fmt = '<dddddddddBBddBddddddBdd'
