d42a54e7cf7f7a1937eaeb85481bdac71b0d1eca
Andrew Straw <strawman@astraw.com>
2014-04-04 06:25:56 +0200

    data2smoothed: skip obj_ids with bad data

--- flydra/a2/data2smoothed.py
+++ flydra/a2/data2smoothed.py
@@ -234,6 +234,9 @@ def convert(infilename,
         except core_analysis.NotEnoughDataToSmoothError:
             #warnings.warn('not enough data to smooth obj_id %d, skipping.'%(obj_id,))
             continue
+        except numpy.linalg.linalg.LinAlgError:
+            warnings.warn('linear algebra error smoothing obj_id %d, skipping.'%(obj_id,))
+            continue
         allrows.append(rows)
         try:
             qualrows = compute_ori_quality(data_file,
