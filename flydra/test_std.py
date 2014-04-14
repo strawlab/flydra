d577e3a3f97bd612b76023956f980ef93743779f
Andrew Straw <strawman@astraw.com>
2014-04-14 23:50:05 +0200

    testing: covert old test to proper nose test

--- flydra/test_std.py
+++ flydra/test_std.py
@@ -1,26 +1,35 @@
 from __future__ import division
 import math
+import numpy as np
 
-# test the fast method of generating a standard deviation
-x=[1,2,3,4,5,6,7,1.2,3.4]
+def calc_online(x):
+    cumsum = 0
+    cumsumsq = 0
+    N = len(x)
+    for i in range(N):
+        cumsum += x[i]
+        cumsumsq += x[i]*x[i]
 
-cumsum = 0
-cumsumsq = 0
-N = len(x)
-for i in range(N):
-    cumsum += x[i]
-    cumsumsq += x[i]*x[i]
-
-mean = cumsum/N
-std = math.sqrt(cumsumsq/N - (mean)**2)
-
-print 'mean, std',mean, std
+    mean = cumsum/N
+    std = math.sqrt(cumsumsq/N - (mean)**2)
+    return mean,std
 
 # compare against the slow method
+def calc_classic(x):
+    cumstd = 0
+    N = len(x)
+    mean = np.mean(x)
+    for i in range(N):
+        cumstd += (x[i]-mean)**2
+    std = math.sqrt( cumstd/(N) )
+    return mean,std
 
-cumstd = 0
-for i in range(N):
-    cumstd += (x[i]-mean)**2
-std = math.sqrt( cumstd/(N) )
+def test_std():
+    # Test the fast method of generating a standard deviation
+    # compatible with online computation.
 
-print 'mean, std',mean, std
+    x=[1,2,3,4,5,6,7,1.2,3.4]
+    m1, s1 = calc_online(x)
+    m2, s2 = calc_classic(x)
+    assert np.allclose(m1,m2)
+    assert np.allclose(s1,s2)
