389347a6af3de1af33a6ae8dfb2ca3775cf4c3fd
Andrew Straw <strawman@astraw.com>
2015-05-16 08:59:34 +0200

    update sympy code. tested with 0.7.3

--- src/refraction_demo.py
+++ src/refraction_demo.py
@@ -1,4 +1,3 @@
-from sympy import *
 """
 
  1--
@@ -31,6 +30,8 @@ n2 is refractive index of medium 2
 z1 is height of point 1
 z2 is depth of point 2
 """
+from sympy import *
+from sympy.solvers.solvers import unrad # tested with sympy==0.7.3
 
 h1 = Symbol('h1', real=True, positive=True)
 h = Symbol('h', real=True, positive=True)
@@ -44,15 +45,7 @@ z2 = Symbol('z2', real=True, positive=True)
 
 duration = n1*sqrt( h1*h1 + z1*z1 ) + n2*sqrt(z2*z2 + h2*h2)
 
-# Minimize the duration by calculating the derivative with respect to
-# h1 and solve for when it is equal to zero.
 ddur_dh1 = diff(duration,h1)
-result = solve(ddur_dh1,h1)
-print 'result', result
-
-# I had to modify sympy 0.7.3 to print this before raising a PolynomialError:
-# Poly((n1**2 - n2**2)*h1**4 +
-#       (-2*h*n1**2 + 2*h*n2**2)*h1**3 +
-#       (h**2*n1**2 - h**2*n2**2 + n1**2*z2**2 - n2**2*z1**2)*h1**2 +
-#       2*h*n2**2*z1**2*h1
-#       - h**2*n2**2*z1**2, h1, domain='ZZ[z1,z2,h,n1,n2]')
+eq, cov, dens = unrad(ddur_dh1)
+poly = Poly(eq,h1)
+print poly
