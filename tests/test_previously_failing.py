fa99db66b87f4831032d1cc58a2ce5666c5d657d
Andrew Straw <strawman@astraw.com>
2015-05-25 00:12:22 +0200

    rename file. fixup: add currently failing test

--- tests/test_previously_failing.py
+++ tests/test_previously_failing.py
@@ -2,7 +2,8 @@ import flydra.reconstruct
 import pkg_resources
 
 def test_failing_quartic_roots():
-    fname = pkg_resources.resource_filename('flydra.a2','sample_calibration2.xml')
+    fname = pkg_resources.resource_filename('flydra.a2',
+                                            'sample_calibration_water.xml')
     R = flydra.reconstruct.Reconstructor(fname)
     cam_id = 'Basler_21426001'
     X3d = (-0.054036740422969506, 0.045115631117431117, -1.4063962542297526e-09)
