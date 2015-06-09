f80e0870f5d1b2c09db9bdc04218eae34f246c7f
Andrew Straw <strawman@astraw.com>
2015-06-09 11:32:52 +0200

    fix h5_shorten command

--- flydra/a2/h5_shorten.py
+++ flydra/a2/h5_shorten.py
@@ -3,9 +3,12 @@ from optparse import OptionParser
 import warnings
 import numpy as np
 
+from flydra.a2.tables_tools import openFileSafe
+import flydra.kalman.flydra_kalman_utils as flydra_kalman_utils
+
 def get_start_stop(src_h5,name,start,stop):
     print '  finding start and stop row of %s'%name
-    input_node = src_h5.root.data2d_distorted
+    input_node = getattr(src_h5.root, name)
     frames = input_node[:]['frame']
     if start is not None:
         valid_cond = frames >= start
@@ -14,23 +17,55 @@ def get_start_stop(src_h5,name,start,stop):
     if stop is not None:
         valid_cond &= frames <= stop
     valid_idx = np.nonzero(valid_cond)[0]
+    start = np.min( valid_idx )
+    stop = np.max( valid_idx )
+    # this may capture some non-relevant rows...
+    return start, stop
 
-    # check that start and stop accurately characterize data
-    di = valid_idx[1:]-valid_idx[:-1]
-    if np.max(di) != 1 or np.min(di) != 1:
-        raise ValueError('data are not contiguous')
+def do_data_association_tables(src_h5, output_file, options):
+    print ('selectively copying relevant rows from "ML_estimates" and '
+           '"ML_estimates_2d_idxs"')
+    # these are more tricky
+    startrow, stoprow = get_start_stop(src_h5, 'ML_estimates',
+                                       options.start, options.stop)
+    orig_ML_est_rows = src_h5.root.ML_estimates[ startrow:stoprow+1 ]
+    new_obs_rows = []
+    new_ML_est_rows = orig_ML_est_rows[:] # copy
+    idxcol = orig_ML_est_rows['obs_2d_idx']
+    for i in range(len(orig_ML_est_rows)):
+        idxidx2d = int(idxcol[i])#orig_ML_est_row['obs_2d_idx']) # XXX pytables fails with np.uint64?
+        idx2d = src_h5.root.ML_estimates_2d_idxs[idxidx2d]
+        new_ML_est_rows['obs_2d_idx'][i] = len( new_obs_rows )
+        new_obs_rows.append(idx2d)
+    # Save data association information.
+    ML_estimates_2d_idxs_type = flydra_kalman_utils.ML_estimates_2d_idxs_type
+    h5_2d_obs = output_file.createVLArray(output_file.root,
+                                          'ML_estimates_2d_idxs',
+                                          ML_estimates_2d_idxs_type(), # dtype should match with tro.observations_2d
+                                          "camns and idxs")
+    for camns_and_idxs in new_obs_rows:
+        h5_2d_obs.append( camns_and_idxs )
+    h5_2d_obs.flush()
 
-    return valid_idx[0], valid_idx[-1]
+    # Save ML_estimates.
+    ct = output_file.createTable # shorthand
+    root = output_file.root # shorthand
+    FilteredObservations = flydra_kalman_utils.FilteredObservations
+    h5data3d_ML_estimates = ct(root,'ML_estimates', FilteredObservations,
+                               'dynamics-free maximum liklihood estimates',
+                               expectedrows=len(new_ML_est_rows))
+    h5data3d_ML_estimates.append(new_ML_est_rows)
+    h5data3d_ML_estimates.flush()
 
 def copy_selective(src_h5,input_node,output_group,options):
     if input_node.name in ['data2d_distorted',
                            'kalman_estimates',
-                           'ML_estimates',
                            ]:
 
-        startrow,stoprow=get_start_stop(src_h5,input_node.name,
-                                        options.start,options.stop)
-        print '  copying data'
+        startrow, stoprow= get_start_stop( src_h5, input_node.name,
+                                           options.start, options.stop)
+        print '  copying data for frames %d - %d (rows %d - %d)' % (
+            options.start, options.stop, startrow, stoprow )
         input_node._f_copy(output_group,
                            start=startrow,
                            stop=stoprow)
@@ -58,22 +93,22 @@ def copy_selective(src_h5,input_node,output_group,options):
                       'table %s'%input_node.name)
         input_node._f_copy(output_group,recursive=True)
 
-def doit(input_filename, output_filename, options):
-    h5 = tables.openFile( input_filename,mode='r')
-    output_h5 = tables.openFile( output_filename,mode='w')
-    for input_node in h5.root._f_iterNodes():
-        print 'copying',input_node
-        if (hasattr(input_node,'name') and
-            input_node.name in ['data2d_distorted',
-                                'kalman_estimates',
-                                'ML_estimates',
-                                'ML_estimates_2d_idxs']):
-            copy_selective(h5,input_node,output_h5.root,options)
-        else:
-            # copy everything from source to dest
-            input_node._f_copy(output_h5.root,recursive=True)
-    h5.close()
-    output_h5.close()
+def h5_shorten(input_filename, output_filename, options):
+    with openFileSafe(input_filename,mode='r') as h5:
+        with openFileSafe( output_filename, mode='w', delete_on_error=True ) as output_h5:
+            do_data_association_tables(h5, output_h5, options)
+            for node in h5.root._f_iterNodes():
+                if (hasattr(node,'name') and
+                    node.name in ['data2d_distorted','kalman_estimates']):
+                    print 'selectively copying',node
+                    copy_selective(h5,node,output_h5.root,options)
+                elif (hasattr(node,'name') and
+                      node.name in ['ML_estimates','ML_estimates_2d_idxs']):
+                    continue
+                else:
+                    # copy everything from source to dest
+                    print 'copying entire',node
+                    node._f_copy(output_h5.root,recursive=True)
 
 def main():
     usage = '%prog [options]'
@@ -88,7 +123,7 @@ def main():
 
     input = options.input
     output = options.output
-    doit(input,output,options)
+    h5_shorten(input,output,options)
 
 if __name__=='__main__':
     main()
