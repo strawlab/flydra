ce38ac8aab59c50a4c71aef64f43eb3981363699
Andrew Straw <strawman@astraw.com>
2021-09-14 01:44:15 +0200

    [flydra_analysis] readme must be in source tarball

--- flydra_analysis/setup.py
+++ flydra_analysis/setup.py
@@ -11,7 +11,7 @@ from os import path
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / ".." / "README.md").read_text('utf-8')
+long_description = (this_directory / "README.md").read_text('utf-8')
 
 ext_modules = []
 
