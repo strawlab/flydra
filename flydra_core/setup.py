4dd51f58af479f75dab217045b97ddaddef23eaf
Andrew Straw <strawman@astraw.com>
2023-07-22 13:59:07 +0200

    flydra-core: fix numpy deprecated C API issue

--- flydra_core/setup.py
+++ flydra_core/setup.py
@@ -9,6 +9,7 @@ ext_modules.append(
         name="flydra_core._reconstruct_utils",
         sources=["flydra_core/_reconstruct_utils.pyx"],
         include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
 )
 
@@ -17,6 +18,7 @@ ext_modules.append(
         name="flydra_core._pmat_jacobian",
         sources=["flydra_core/_pmat_jacobian.pyx"],
         include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
 )
 
@@ -25,6 +27,7 @@ ext_modules.append(
         name="flydra_core._pmat_jacobian_water",
         sources=["flydra_core/_pmat_jacobian_water.pyx"],
         include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
 )
 
@@ -33,26 +36,28 @@ ext_modules.append(
         name="flydra_core._flydra_tracked_object",
         sources=["flydra_core/_flydra_tracked_object.pyx"],
         include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
 )
 
 ext_modules.append(
-    Extension(name="flydra_core._mahalanobis", sources=["flydra_core/_mahalanobis.pyx"])
+    Extension(name="flydra_core._mahalanobis", sources=["flydra_core/_mahalanobis.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],)
 )
 
 ext_modules.append(
-    Extension(name="flydra_core._fastgeom", sources=["flydra_core/_fastgeom.pyx"])
+    Extension(name="flydra_core._fastgeom", sources=["flydra_core/_fastgeom.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],)
 )
 
 ext_modules.append(
     Extension(
         name="flydra_core._Roots3And4",
         sources=["flydra_core/_Roots3And4.pyx", "flydra_core/Roots3And4.c",],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
 )
 
 ext_modules.append(
-    Extension(name="flydra_core._refraction", sources=["flydra_core/_refraction.pyx"])
+    Extension(name="flydra_core._refraction", sources=["flydra_core/_refraction.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],)
 )
 
 setup(
